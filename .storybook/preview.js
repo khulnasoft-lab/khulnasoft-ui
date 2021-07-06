324d14106fe77465874610011887774f04f4b9b9
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-07-06 14:20:17 -0400

    chore: make stories embeddable
    This does some changes to make stories embeddable outside of GitLab
    UI's Storybook:
    - Adds the `iframe-resizer` script.
    - Add support for an isEmbeddedStory URL param to force stories to use
      the fullscreen layout.
    - Remove top and bottom padding in docs pages.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,9 @@ import { addParameters, addDecorator } from '@storybook/vue';
 import { addReadme } from 'storybook-readme/vue';
 import { setupStorybookReadme } from '../documentation/documented_stories';
 
+const searchParams = new URL(document.location).searchParams;
+const isEmbeddedStory = Boolean(searchParams.get('isEmbeddedStory'));
+
 const stylesheetsRequireCtx = require.context('../src/scss', true, /(storybook|bootstrap)\.scss$/);
 
 stylesheetsRequireCtx('./bootstrap.scss');
@@ -31,12 +34,15 @@ export const parameters = {
  * - Skip storybook-readme's setup to avoid rendering the READMEs.
  * - Skip DocsPage settings to prevent JSX errors.
  * - Set the layout to fullscreen to ensure stories are full-width.
+ * The layout is also set to fullscreen whenever the isEmbeddedStory search param is passed.
+ * This lets us remove unnecessary spacing when embedding stories in design.gitlab.com.
  */
 if (process.env.NODE_ENV !== 'test') {
   setupStorybookReadme();
   const { page } = require('./docs/page');
   parameters.docs = { page };
-} else {
+}
+if (process.env.NODE_ENV === 'test' || isEmbeddedStory) {
   parameters.layout = 'fullscreen';
 }
 
