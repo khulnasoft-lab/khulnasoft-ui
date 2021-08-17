fd8fe117c5bc40772b53d534b131ef3db5be27c0
Lee Tickett <lee@tickett.net>
2021-08-17 10:07:58 +0100

    chore(GlButtonGroup): Migrate stories to CSF
    Closes https://gitlab.com/gitlab-org/gitlab-ui/-/issues/1329.

--- src/components/base/button_group/button_group.documentation.js
+++ src/components/base/button_group/button_group.documentation.js
@@ -1,9 +1,7 @@
 import description from './button_group.md';
-import examples from './examples';
 
 export default {
   description,
-  examples,
   followsDesignSystem: true,
   bootstrapComponent: 'b-button-group',
 };
