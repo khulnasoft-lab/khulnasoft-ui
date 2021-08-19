918c68b3b8147430d6deadb6e8e490076cade529
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-08-19 14:54:11 +0200

    chore(GlToggle): migrate stories to CSF
    This migrates GlToggle'stories to the Component Story Format.

--- src/components/base/toggle/toggle.documentation.js
+++ src/components/base/toggle/toggle.documentation.js
@@ -1,20 +1,6 @@
-import examples from './examples';
 import * as description from './toggle.md';
 
 export default {
   description,
   followsDesignSystem: true,
-  examples,
-  events: [
-    {
-      event: 'change',
-      description: 'Emitted when the state changes',
-      args: [
-        {
-          arg: 'value',
-          description: 'Whether the toggle is in the enabled',
-        },
-      ],
-    },
-  ],
 };
