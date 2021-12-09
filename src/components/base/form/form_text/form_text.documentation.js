b64bc6239ed5421b2c2023149e6fc59da936256e
Shreedhar Bhat <shridharbhat1998@gmail.com>
2021-12-09 13:07:26 +0000

    feat(GlFormText): Migrate storybook story

--- src/components/base/form/form_text/form_text.documentation.js
+++ src/components/base/form/form_text/form_text.documentation.js
@@ -1,9 +1,7 @@
-import examples from './examples';
 import * as description from './form_text.md';
 
 export default {
   description,
-  examples,
   bootstrapComponent: 'b-form-text',
   propsInfo: {},
 };
--- src/components/base/form/form_text/form_text.md
+++ src/components/base/form/form_text/form_text.md
@@ -1,6 +1,4 @@
-# FormText
-
-<!-- STORY -->
+# Usage
 
 The component used to render form group descriptions. Useful for when you need
 to add additional form text elements to a form group.
