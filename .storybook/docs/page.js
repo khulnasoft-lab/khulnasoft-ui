7083d5a7aae9ca21e2fc8b156dac9ea9295aa291
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2021-12-10 12:00:28 +0000

    chore: add LinkToSource docs block

--- .storybook/docs/page.js
+++ .storybook/docs/page.js
@@ -8,12 +8,14 @@ import {
   Stories,
   PRIMARY_STORY,
 } from '@storybook/addon-docs';
-import { ImportInfo } from './blocks/ImportInfo';
-import { BootstrapComponent } from './blocks/BootstrapComponent';
+import { ImportInfo, BootstrapComponent, LinkToSource } from './blocks';
 
 export const page = () => (
   <>
-    <Title />
+    <div className="gl-display-flex gl-align-items-center">
+      <Title />
+      <LinkToSource className="gl-ml-auto!" />
+    </div>
     <Subtitle />
     <ImportInfo />
     <Description />
