07205105cb98e8493cfe3f8b70191707175ca8a9
Shreedhar Bhat <shridharbhat1998@gmail.com>
2021-12-13 14:45:27 +0000

    feat(GlAvatarsInline): Migrate storybook story

--- src/components/base/avatars_inline/avatars_inline.stories.js
+++ src/components/base/avatars_inline/avatars_inline.stories.js
@@ -1,7 +1,4 @@
-import { number, boolean, object, select, withKnobs } from '@storybook/addon-knobs';
-import { documentedStoriesOf } from '../../../../documentation/documented_stories';
 import { avatarsInlineSizeOptions } from '../../../utils/constants';
-
 import readme from './avatars_inline.md';
 
 const defaultAvatars = [
@@ -12,53 +9,61 @@ const defaultAvatars = [
   { src: 'https://picsum.photos/id/1012/32' },
 ];
 
-function generateProps(avatars = defaultAvatars) {
-  const props = {
-    maxVisible: {
-      type: Number,
-      default: number('maxVisible', 2),
-    },
-    collapsed: {
-      type: Boolean,
-      default: boolean('collapsed', true),
-    },
-    size: {
-      type: Number,
-      default: select('size', avatarsInlineSizeOptions, 24),
-    },
-    avatars: {
-      type: Array,
-      default: object('avatars', avatars),
-    },
-  };
+const generateProps = (
+  avatars = defaultAvatars,
+  { maxVisible = 2, collapsed = true, avatarSize = 24 } = {}
+) => ({
+  maxVisible,
+  collapsed,
+  avatarSize,
+  avatars,
+});
 
-  return props;
-}
-
-documentedStoriesOf('base/avatar/avatars-inline', readme)
-  .addDecorator(withKnobs)
-  .add('default', () => ({
-    props: generateProps(),
-    template: `
-    <gl-avatars-inline :avatars="avatars" :collapsed="collapsed" :avatar-size="size" :max-visible="maxVisible">
+export const Default = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  template: `
+    <gl-avatars-inline :avatars="avatars" :collapsed="collapsed" :avatar-size="avatarSize" :max-visible="maxVisible">
     </gl-avatars-inline>
     `,
-  }))
-  .add('with links and tooltips', () => ({
-    props: generateProps(
-      defaultAvatars.map((avatar, index) => ({
-        ...avatar,
-        href: '//gitlab.com',
-        tooltip: `Avatar ${index}`,
-      }))
-    ),
-    template: `
-    <gl-avatars-inline :avatars="avatars" :collapsed="collapsed" :avatar-size="size" :max-visible="maxVisible">
+});
+Default.args = generateProps();
+
+export const WithLinksAndTooltips = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  template: `
+    <gl-avatars-inline :avatars="avatars" :collapsed="collapsed" :avatar-size="avatarSize" :max-visible="maxVisible">
       <template #avatar="{ avatar }">
         <gl-avatar-link target="blank" :href="avatar.href" v-gl-tooltip :title="avatar.tooltip">
-          <gl-avatar :src="avatar.src" :size="size" />
+          <gl-avatar :src="avatar.src" :size="avatarSize" />
         </gl-avatar-link>
       </template>
     </gl-avatars-inline>
     `,
-  }));
+});
+WithLinksAndTooltips.args = generateProps(
+  defaultAvatars.map((avatar, index) => ({
+    ...avatar,
+    href: '//gitlab.com',
+    tooltip: `Avatar ${index}`,
+  }))
+);
+
+export default {
+  title: 'base/avatar/avatars-inline',
+  parameters: {
+    knobs: { disable: true },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  argTypes: {
+    avatarSize: {
+      control: {
+        type: 'select',
+        options: avatarsInlineSizeOptions,
+      },
+    },
+  },
+};
