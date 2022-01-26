697a0f3b70da72de0800d327b36579e6f4ec3bde
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-01-26 11:59:45 +0100

    refactor: leverage findByTestId in integration tests
    Use the `findByTestId` custom command in Cypress integration tests.

--- cypress/integration/accordion_spec.js
+++ cypress/integration/accordion_spec.js
@@ -3,17 +3,17 @@ describe('GlAccordion', () => {
     cy.visitStory('accordion');
   });
 
-  const accordionContentId = '[data-testid="accordion-item-collapse-accordion-item-2"]';
+  const accordionContentId = 'accordion-item-collapse-accordion-item-2';
 
   it('clicking on collapsed chevron icon expands accordion item then collapses when clicked again', () => {
-    cy.get(accordionContentId).should('not.be.visible');
+    cy.findByTestId(accordionContentId).should('not.be.visible');
     cy.contains('Item 1').click();
-    cy.get(accordionContentId).should('be.visible');
+    cy.findByTestId(accordionContentId).should('be.visible');
   });
 
   it('clicking on expanded chevron icon collapses accordion item', () => {
-    cy.get(accordionContentId).should('be.visible');
+    cy.findByTestId(accordionContentId).should('be.visible');
     cy.contains('Item 1').click();
-    cy.get(accordionContentId).should('not.be.visible');
+    cy.findByTestId(accordionContentId).should('not.be.visible');
   });
 });
--- cypress/integration/popover_spec.js
+++ cypress/integration/popover_spec.js
@@ -2,14 +2,14 @@ describe('GlPopover', () => {
   it('popover title should be visible when using props', () => {
     cy.visitStory('popover');
 
-    cy.get('[data-testid="popover-with-props"]').contains('Popover');
+    cy.findByTestId('popover-with-props').contains('Popover');
   });
 
   it('popover title should be visible when using scoped slot', () => {
     cy.visit('iframe.html?id=base-popover--on-click&viewMode=story');
 
-    cy.get('[data-testid="popover-button-click"]').click();
+    cy.findByTestId('popover-button-click').click();
 
-    cy.get('[data-testid="popover-title"]').should('be.visible').contains('Popover title');
+    cy.findByTestId('popover-title').should('be.visible').contains('Popover title');
   });
 });
