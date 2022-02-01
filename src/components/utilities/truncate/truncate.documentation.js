e0a21b8016affa16391975ac97f51327270cc782
Rishabh Gupta <belikerishabh@gmail.com>
2022-02-01 08:43:12 +0000

    refactor: Migrating utilities/truncate to CSF

--- src/components/utilities/truncate/truncate.documentation.js
+++ src/components/utilities/truncate/truncate.documentation.js
@@ -1,19 +1,5 @@
-import examples from './examples';
 import description from './truncate.md';
 
 export default {
   description,
-  examples,
-  propsInfo: {
-    text: {
-      additionalInfo: 'Text to be ellipsized',
-    },
-    position: {
-      additionalInfo: 'Ellipsis position',
-      enum: 'truncateOptions',
-    },
-    withTooltip: {
-      additionalInfo: 'Display the full text in a tooltip only if it is being truncated',
-    },
-  },
 };
