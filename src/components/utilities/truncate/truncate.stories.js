ed4aa40ed8c3e748f78e78d2e312a8be9ffb9833
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2022-03-17 16:09:27 +0000

    revert(GlTruncate): add slots to surround the text
    This reverts merge request 2536 as we have determined the issue it was
    meant to solve could be addressed in the consuming projects without
    adding the extra complexity.
    
    BREAKING CHANGE: This drops the before-text and after-text slots from
    GlTruncate.

--- src/components/utilities/truncate/truncate.stories.js
+++ src/components/utilities/truncate/truncate.stories.js
@@ -1,21 +1,8 @@
-import { GlIcon, GlTruncate } from '../../../index';
+import { GlTruncate } from '../../../index';
 import { POSITION } from './constants';
 import readme from './truncate.md';
 
 const template = '<gl-truncate :text="text" :position="position" :with-tooltip="withTooltip" />';
-const beforeTextTemplate = `
-<gl-truncate :text="text" :position="position" :with-tooltip="withTooltip">
-  <template #before-text><gl-icon name="container-image" /></template>
-</gl-truncate>`;
-const afterTextTemplate = `
-<gl-truncate :text="text" :position="position" :with-tooltip="withTooltip">
-  <template #after-text><gl-icon name="abuse" /></template>
-</gl-truncate>`;
-const bothSlotsTemplate = `
-<gl-truncate :text="text" :position="position" :with-tooltip="withTooltip">
-  <template #before-text><gl-icon name="rocket" /></template>
-  <template #after-text><gl-icon name="requirements" /></template>
-</gl-truncate>`;
 
 const generateProps = ({
   text = 'src/thisIs/AVeryLongFilePath/that/needs/to/be/smartly/truncated/from/the/middle/so/we/dont/lose/important/information/here.vue',
@@ -36,51 +23,6 @@ const Template = (args, { argTypes }) => ({
 });
 export const Default = Template.bind({});
 Default.args = generateProps();
-
-const BeforeTextTemplate = (args, { argTypes }) => ({
-  components: { GlIcon, GlTruncate },
-  props: Object.keys(argTypes),
-  template: beforeTextTemplate,
-});
-export const BeforeText = BeforeTextTemplate.bind({});
-BeforeText.args = generateProps({ position: 'start' });
-BeforeText.parameters = {
-  docs: {
-    source: {
-      code: beforeTextTemplate,
-    },
-  },
-};
-
-const AfterTextTemplate = (args, { argTypes }) => ({
-  components: { GlIcon, GlTruncate },
-  props: Object.keys(argTypes),
-  template: afterTextTemplate,
-});
-export const AfterText = AfterTextTemplate.bind({});
-AfterText.args = generateProps({ position: 'end' });
-AfterText.parameters = {
-  docs: {
-    source: {
-      code: afterTextTemplate,
-    },
-  },
-};
-
-const BothSlotsTemplate = (args, { argTypes }) => ({
-  components: { GlIcon, GlTruncate },
-  props: Object.keys(argTypes),
-  template: bothSlotsTemplate,
-});
-export const BothSlots = BothSlotsTemplate.bind({});
-BothSlots.args = generateProps();
-BothSlots.parameters = {
-  docs: {
-    source: {
-      code: bothSlotsTemplate,
-    },
-  },
-};
 
 export default {
   title: 'utilities/truncate',
