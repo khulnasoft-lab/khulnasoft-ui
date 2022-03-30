9419cb342aa830cf7e9733d947f2e68f7c19aa6b
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-03-30 14:29:55 -0400

    chore: set config in preview.js

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,8 +1,5 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
-import setConfigs from '../src/config';
-
-setConfigs();
 
 const theme = create({
   base: 'light',
