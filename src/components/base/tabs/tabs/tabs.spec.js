99cb9944b651ba048cfd3dd1266b769dd095aeec
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-05-03 18:09:49 -0400

    chore(GlTabs): wait next tick when setting active tab from query
    This delays the active tab's initialization by one tick. This is
    required after some change in bootstrap-vue 2.21.1 that also delays the
    active tab's initialization. We need to make sure our logic is triggered
    after bootstrap-vue's to override its behavior and not the other way
    around.
    https://github.com/bootstrap-vue/bootstrap-vue/commit/6d92a4376c227a02a7c24e19c04e437bb8909c1c

--- src/components/base/tabs/tabs/tabs.spec.js
+++ src/components/base/tabs/tabs/tabs.spec.js
@@ -229,6 +229,7 @@ describe('tabs component', () => {
             });
 
             await nextTick();
+            await nextTick();
 
             expect(findActiveTab().text()).toBe(expectedActiveTabText);
 
@@ -338,9 +339,10 @@ describe('tabs component', () => {
           });
 
           await nextTick();
+          await nextTick();
 
-          expect(wrapper.emitted('input')[0]).toEqual([1]);
-          expect(wrapper.emitted('input')[1]).toEqual([1]);
+          expect(wrapper.emitted().input[0]).toEqual([0]);
+          expect(wrapper.emitted().input[1]).toEqual([1]);
         });
       });
     });
