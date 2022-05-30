9217ee42c9b54e04a8c98c56c50f9018896bc32d
Ezekiel Kigbo <ekigbo@gitlab.com>
2022-05-30 13:33:27 +1000

    feat(GlSingleStat): support string and number for value prop
    Adds support for the number data type for
    the value prop.

--- src/components/charts/single_stat/single_stat.vue
+++ src/components/charts/single_stat/single_stat.vue
@@ -16,11 +16,11 @@ export default {
       required: true,
     },
     value: {
-      type: String,
+      type: [String, Number],
       required: true,
     },
     unit: {
-      type: [String, Number],
+      type: String,
       required: false,
       default: null,
     },
