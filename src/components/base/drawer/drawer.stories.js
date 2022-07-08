29c86f5fb0df6c070acb2eee26c85cfab3b831f4
Mark Florian <mflorian@gitlab.com>
2022-07-08 10:22:34 +0100

    chore(GlDrawer): Use simpler array creation in story

--- src/components/base/drawer/drawer.stories.js
+++ src/components/base/drawer/drawer.stories.js
@@ -5,30 +5,30 @@ import readme from './drawer.md';
 const components = { GlDrawer, GlButton, GlMarkdown };
 
 const generateStaticContent = (number = 1) =>
-  Array.from(Array(number).keys())
-    .map(
-      (item) => `
+  Array.from(
+    Array(number),
+    (_, index) => `
       <div class="gl-mb-8">
-            <h4 class="gl-mb-4">View jobs in a pipeline ${item}</h4>
-            <p>
-              Pipeline configuration begins with jobs. Jobs are the most fundamental element of a .gitlab-ci.yml file.
-            </p>
-            <p>Jobs are:</p>
-            <ul>
-              <li>Defined with constraints stating under what conditions they should be executed. </li>
-              <li>Top-level elements with an arbitrary name and must contain at least the script clause.</li>
-              <li>Not limited in how many can be defined.</li>
-            </ul>
-            <p>For example:</p>
-            <gl-markdown compact>
-              <code>job1: script: "execute-script-for-job1"</code>
-              <br />
-              <code>job2: script: "execute-script-for-job2"</code>
-            </gl-markdown>
-          </div>
+        <h4 class="gl-mb-4">View jobs in a pipeline ${index}</h4>
+        <p>
+          Pipeline configuration begins with jobs. Jobs are the most fundamental element of a .gitlab-ci.yml file.
+        </p>
+        <p>Jobs are:</p>
+        <ul>
+          <li>Defined with constraints stating under what conditions they should be executed. </li>
+          <li>Top-level elements with an arbitrary name and must contain at least the script clause.</li>
+          <li>Not limited in how many can be defined.</li>
+        </ul>
+        <p>For example:</p>
+        <gl-markdown compact>
+          <code>job1: script: "execute-script-for-job1"</code>
+          <br />
+          <code>job2: script: "execute-script-for-job2"</code>
+        </gl-markdown>
+      </div>
     `
-    )
-    .join('');
+  ).join('');
+
 const generateDrawerContent = (items) =>
   items
     .map(
