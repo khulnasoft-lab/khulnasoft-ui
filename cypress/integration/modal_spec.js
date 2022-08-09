bbfe03e41435f292893bab08af78c6bbce98e4df
Mark Florian <mflorian@gitlab.com>
2022-08-09 12:55:49 +0100

    test(GlModal): Use visitStory command

--- cypress/integration/modal_spec.js
+++ cypress/integration/modal_spec.js
@@ -1,24 +1,24 @@
 describe('GlModal', () => {
   it('Modal title should be visible using a scoped slot', () => {
-    cy.visit('iframe.html?id=base-modal--with-a-header&viewMode=story');
+    cy.visitStory('modal', 'with-a-header');
 
     cy.get('#test-modal-id H4').should('be.visible').contains('A custom header');
   });
 
   it('Modal footer should not be visible using a scoped slot', () => {
-    cy.visit('iframe.html?id=base-modal--without-a-footer&viewMode=story');
+    cy.visitStory('modal', 'without-a-footer');
 
     cy.get('#test-modal-id').should('not.have.class', 'modal-footer');
   });
 
   it('focused first focusable element by default', () => {
-    cy.visit('iframe.html?id=base-modal--default');
+    cy.visitStory('modal');
 
     cy.contains('button', 'Cancel').should('be.focused');
   });
 
   it('does not focus first focusable element when noFocusOnShow prop is true', () => {
-    cy.visit('iframe.html?id=base-modal--without-focus');
+    cy.visitStory('modal', 'without-focus');
 
     cy.contains('button', 'Cancel').should('not.be.focused');
   });
