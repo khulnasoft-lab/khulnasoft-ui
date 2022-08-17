1e80adc82acfbd84afacfba10ae4d6860ef57996
Shreedhar Bhat <shridharbhat1998@gmail.com>
2022-08-17 18:57:56 +0530

    feat(GlFormSetlect): Allowed to cofigure select size
    Passed a optional param size to GlFormSelect
    
    Resolves the issue https://gitlab.com/gitlab-org/gitlab-ui/-/issues/1870

--- src/components/base/form/form_select/form_select.vue
+++ src/components/base/form/form_select/form_select.vue
@@ -1,15 +1,52 @@
 <script>
 import { BFormSelect } from 'bootstrap-vue';
+import { isObject } from 'lodash';
+import { formInputSizes } from '../../../../utils/constants';
 
 export default {
   components: {
     BFormSelect,
   },
   inheritAttrs: false,
+  props: {
+    /**
+     * Maximum width of the Select
+     */
+    size: {
+      type: [String, Object],
+      required: false,
+      default: null,
+      validator: (value) => {
+        const sizes = isObject(value) ? Object.values(value) : [value];
+
+        return sizes.every((size) => Object.values(formInputSizes).includes(size));
+      },
+    },
+  },
+  computed: {
+    cssClasses() {
+      if (this.size === null) {
+        return [];
+      }
+
+      if (isObject(this.size)) {
+        const { default: defaultSize, ...nonDefaultSizes } = this.size;
+
+        return [
+          ...(defaultSize ? [`gl-form-select-${defaultSize}`] : []),
+          ...Object.entries(nonDefaultSizes).map(
+            ([breakpoint, size]) => `gl-${breakpoint}-form-select-${size}`
+          ),
+        ];
+      }
+
+      return [`gl-form-select-${this.size}`];
+    },
+  },
 };
 </script>
 <template>
-  <b-form-select class="gl-form-select" v-bind="$attrs" v-on="$listeners">
+  <b-form-select class="gl-form-select" v-bind="$attrs" :class="cssClasses" v-on="$listeners">
     <!-- eslint-disable-next-line @gitlab/vue-prefer-dollar-scopedslots -->
     <template v-for="slot in Object.keys($slots)" #[slot]>
       <slot :name="slot"></slot>
