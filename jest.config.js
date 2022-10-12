3c10231988314e47eeb41298b3fb2c952ed7c885
Illya Klymov <iklymov@gitlab.com>
2022-10-13 00:11:57 +0300

    chore(ci): introduce @vue/compat testing infrastucture
    * add VUE_VERSION=3 env variable to run code with Vue.js 3

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,39 @@
 const reporters = ['default'];
+const setupFilesAfterEnv = ['<rootDir>/tests/jest_setup.js'];
+
+const USE_VUE_3 = process.env.VUE_VERSION === '3';
+const VUE_JEST_TRANSFORMER = USE_VUE_3 ? '@vue/vue3-jest' : '@vue/vue2-jest';
+
+const customModuleNameMappers = {};
+const extraJestConfig = {};
+
+if (USE_VUE_3) {
+  global.console.log('Using Vue.js 3');
+
+  setupFilesAfterEnv.unshift('<rootDir>/tests/jest_setup_vue3_compat.js');
+  Object.assign(customModuleNameMappers, {
+    '^vue$': '@vue/compat',
+    '^@vue/test-utils$': '@vue/test-utils-vue3',
+  });
+  Object.assign(extraJestConfig, {
+    globals: {
+      'vue-jest': {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2,
+          },
+        },
+      },
+    },
+  });
+}
 
 if (process.env.JEST_IMAGE_SNAPSHOT_TRACK_OBSOLETE) {
   reporters.push('jest-image-snapshot/src/outdated-snapshot-reporter.js');
 }
 
 module.exports = {
+  ...extraJestConfig,
   verbose: true,
   moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
@@ -12,21 +41,22 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/src/$1',
     '^@gitlab/ui$': '<rootDir>/index.js',
     '\\.(css|scss|less)$': 'identity-obj-proxy',
+    ...customModuleNameMappers,
   },
   modulePathIgnorePatterns: ['cypress/integration', '.cypress_cache'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': '@vue/vue2-jest',
+    '.*\\.(vue)$': VUE_JEST_TRANSFORMER,
     '\\.(svg|html|md|png)$': '<rootDir>/tests/transformers/file_transformer.js',
   },
   transformIgnorePatterns: [
-    '/node_modules(?![\\\\/]bootstrap-vue[\\\\/]|(/@storybook/.*\\.vue$)|(/@gitlab/svgs/))/',
+    '/node_modules(?![\\\\/]bootstrap-vue[\\\\/]|(/@storybook/.*\\.vue$)|(/@gitlab/svgs/)|(/vue-test-utils-compat/))/',
   ],
   snapshotSerializers: [
     '<rootDir>/tests/html_string_serializer.js',
     '<rootDir>/tests/clean_html_element_serializer.js',
   ],
-  setupFilesAfterEnv: ['<rootDir>/tests/jest_setup.js'],
+  setupFilesAfterEnv,
   reporters,
   testEnvironment: 'jsdom',
   testRunner: 'jest-circus/runner',
