25480e815856a07795834b24a3729695390087c6
Illya Klymov <iklymov@gitlab.com>
2022-11-03 05:18:39 +0200

    chore(tests): update serializer based on feedback
    * separate in two serializers
    * do not use prettier

---
+++ tests/html_string_serializer.js
@@ -0,0 +1,12 @@
+export function test(received) {
+  // TODO, maybe there's a better way to determine if a string is intended to be HTML
+  return received && typeof received === 'string' && received.startsWith('<');
+}
+
+export function serialize(received, config, indentation, depth, refs, printer) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(received, 'text/html');
+  const el = doc.body.firstElementChild;
+
+  return printer(el, config, indentation, depth, refs);
+}
