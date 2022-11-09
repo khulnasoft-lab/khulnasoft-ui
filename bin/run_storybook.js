b6b4c7f9ea0ee0acf878b2a2e3156ab029ed011f
Illya Klymov <iklymov@gitlab.com>
2022-11-09 12:48:03 +0200

    chore(vue3): add vue3 support for storybook
    * introduce vue3-related infrastructure for storybook
    * puts all Vue 3 checks and constants into one file, making it easier
      to see affected areas

---
+++ bin/run_storybook.js
@@ -0,0 +1,4 @@
+const { STORYBOOK_VUE_FRAMEWORK } = require('../use_vue3');
+
+// eslint-disable-next-line import/no-dynamic-require
+require(`${STORYBOOK_VUE_FRAMEWORK}/bin/index.js`);
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
+const { USE_VUE_3, JEST_VUE_TRANSFORMER } = require('./use_vue3');
+
 const reporters = ['default'];
 const setupFilesAfterEnv = ['<rootDir>/tests/jest_setup.js'];
-
-const USE_VUE_3 = process.env.VUE_VERSION === '3';
-const VUE_JEST_TRANSFORMER = USE_VUE_3 ? '@vue/vue3-jest' : '@vue/vue2-jest';
 
 const customModuleNameMappers = {};
 const extraJestConfig = {};
@@ -46,7 +45,7 @@ module.exports = {
   modulePathIgnorePatterns: ['cypress/integration', '.cypress_cache'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': VUE_JEST_TRANSFORMER,
+    '.*\\.(vue)$': JEST_VUE_TRANSFORMER,
     '\\.(svg|html|md|png)$': '<rootDir>/tests/transformers/file_transformer.js',
   },
   transformIgnorePatterns: [
