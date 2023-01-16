6a8aa55faea07e6afe15dbed2033031bb5f4d21c
Lukas Eipert <leipert@gitlab.com>
2023-01-16 16:52:28 +0100

    chore: Fix font paths in storybook CSS
    If we import them directly and teach webpack what to do with the woff
    files, the issues should be fixed.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -64,7 +64,7 @@ module.exports = ({ config }) => {
       loader: 'file-loader',
     },
     {
-      test: /\/static\/img\//,
+      test: /\/static\/(img|fonts)\//,
       loader: 'file-loader',
       options: {
         outputPath: 'static',
