6fa40594c1273033140f5531812d223b403dfa4d
Alexander Turinske <aturinske@gitlab.com>
2023-02-06 12:27:52 +0000

    chore: Alphabetize key constants

--- src/components/base/new_dropdowns/constants.js
+++ src/components/base/new_dropdowns/constants.js
@@ -14,9 +14,9 @@ export const GL_DROPDOWN_SHOWN = 'shown';
 export const GL_DROPDOWN_HIDDEN = 'hidden';
 
 // KEY Codes
-export const HOME = 'Home';
-export const END = 'End';
-export const ARROW_UP = 'ArrowUp';
 export const ARROW_DOWN = 'ArrowDown';
+export const ARROW_UP = 'ArrowUp';
+export const END = 'End';
 export const ENTER = 'Enter';
+export const HOME = 'Home';
 export const SPACE = 'Space';
