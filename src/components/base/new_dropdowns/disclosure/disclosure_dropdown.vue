4896b9c3a6f9075a76fa71ea275f2153de111249
jrushford <jrushford@gitlab.com>
2023-10-12 17:55:28 +0200

    feat(DisclosureDropdown): Add startOpened prop

--- src/components/base/new_dropdowns/disclosure/disclosure_dropdown.vue
+++ src/components/base/new_dropdowns/disclosure/disclosure_dropdown.vue
@@ -217,6 +217,14 @@ export default {
       default: POSITION_ABSOLUTE,
       validator: (strategy) => [POSITION_ABSOLUTE, POSITION_FIXED].includes(strategy),
     },
+    /**
+     * Opens dropdown on render
+     */
+    startOpened: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
   },
   data() {
     return {
@@ -234,6 +242,11 @@ export default {
     hasCustomToggle() {
       return Boolean(this.$scopedSlots.toggle);
     },
+  },
+  mounted() {
+    if (this.startOpened) {
+      this.open();
+    }
   },
   methods: {
     open() {
