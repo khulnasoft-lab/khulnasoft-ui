7ea7fd9010a4495be8ce9104d92247b27084ba47
Mark Florian <mflorian@gitlab.com>
2023-10-13 13:42:08 +0100

    test(GlSorting): Clean up tests
    This removes assertions against instance internals. These tests already
    verify the same information on the rendered output, which is sufficient.
    Testing Vue instance internals is a code smell.
    
    This also removes unnecessary object spreads, which are applied anyway
    in the createComponent function.

--- src/components/base/sorting/sorting.spec.js
+++ src/components/base/sorting/sorting.spec.js
@@ -30,14 +30,12 @@ describe('sorting component', () => {
   it('should display default text in dropdown', () => {
     createComponent();
 
-    expect(wrapper.vm.text).toBe(defaultDropdownText);
     expect(selectDropdownButton().text()).toBe(defaultDropdownText);
   });
 
   it('should have a default sort direction of desc and displays the descending icon', () => {
     createComponent();
 
-    expect(wrapper.vm.isAscending).toBe(false);
     expect(selectDirectionButton().findComponent(GlIcon).props('name')).toBe('sort-highest');
   });
 
@@ -45,21 +43,17 @@ describe('sorting component', () => {
     const newDropdownText = 'Some new text';
 
     createComponent({
-      ...defaultProps,
       text: newDropdownText,
     });
 
-    expect(wrapper.vm.text).toBe(newDropdownText);
     expect(selectDropdownButton().text()).toBe(newDropdownText);
   });
 
   it('should accept isAscending true as a default sort direction and display the ascending icon', () => {
     createComponent({
-      ...defaultProps,
       isAscending: true,
     });
 
-    expect(wrapper.vm.isAscending).toBe(true);
     expect(selectDirectionButton().findComponent(GlIcon).props('name')).toBe('sort-lowest');
   });
 
@@ -75,17 +69,14 @@ describe('sorting component', () => {
     const newDirectionTooltip = 'New tooltip text';
 
     createComponent({
-      ...defaultProps,
       sortDirectionToolTip: newDirectionTooltip,
     });
 
-    expect(wrapper.vm.sortDirectionToolTip).toBe(newDirectionTooltip);
     expect(selectDirectionButton().attributes('title')).toBe(newDirectionTooltip);
   });
 
   it('adds classes passed in `dropdownClass` prop to dropdown', () => {
     createComponent({
-      ...defaultProps,
       dropdownClass: 'foo-bar',
     });
 
@@ -94,7 +85,6 @@ describe('sorting component', () => {
 
   it('adds classes passed in `dropdownToggleClass` prop to dropdown toggle', () => {
     createComponent({
-      ...defaultProps,
       dropdownToggleClass: 'foo-bar',
     });
 
@@ -103,7 +93,6 @@ describe('sorting component', () => {
 
   it('adds classes passed in `sortDirectionToggleClass` prop to sort direction toggle', () => {
     createComponent({
-      ...defaultProps,
       sortDirectionToggleClass: 'foo-bar',
     });
 
