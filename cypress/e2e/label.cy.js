46efe05dcefe78ea365054bd5568e0ccd6131f79
Enrique Alcantara <ealcantara@gitlab.com>
2023-10-26 09:43:34 +0200

    test(GlLabel): Add a11y axe tests to GlLabel component

---
+++ cypress/e2e/label.cy.js
@@ -0,0 +1,60 @@
+describe('GlLabel', () => {
+  function checkA11YDefaultState() {
+    cy.visitStory('base/label');
+
+    cy.glCheckA11y();
+  }
+
+  function checkA11YFocusedLabelState() {
+    cy.visitStory('base/label');
+
+    cy.get('.gl-label a').focus();
+
+    cy.glCheckA11y();
+  }
+
+  function checkA11YDarkBackgroundColors() {
+    cy.visitStory('base/label', {
+      args: {
+        backgroundColor: '!hex(785d5d)',
+      },
+    });
+
+    cy.glCheckA11y();
+  }
+
+  function checkA11YLightBackgroundColors() {
+    cy.visitStory('base/label', {
+      args: {
+        backgroundColor: '!hex(e8d8d8)',
+      },
+    });
+
+    cy.glCheckA11y();
+  }
+
+  function checkA11YScopedLabel() {
+    cy.visitStory('base/label', {
+      story: 'scoped',
+    });
+
+    cy.glCheckA11y();
+  }
+
+  function checkA11YLabelWithCloseButton() {
+    cy.visitStory('base/label', {
+      story: 'with-close-button',
+    });
+
+    cy.glCheckA11y();
+  }
+
+  it('passes axe accessibility audits', () => {
+    checkA11YDefaultState();
+    checkA11YFocusedLabelState();
+    checkA11YDarkBackgroundColors();
+    checkA11YLightBackgroundColors();
+    checkA11YScopedLabel();
+    checkA11YLabelWithCloseButton();
+  });
+});
