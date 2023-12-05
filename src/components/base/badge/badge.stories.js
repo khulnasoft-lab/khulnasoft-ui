e13f61ac2d726fa8878a0d7aa528b3d63882d838
Julia Miocene <jmiocene@gitlab.com>
2023-12-05 12:43:49 +0000

    chore(GlBadge): update icon stories

--- src/components/base/badge/badge.stories.js
+++ src/components/base/badge/badge.stories.js
@@ -14,7 +14,8 @@ const template = `
       :variant="variant"
       :size="size"
       :icon="icon"
-      :iconSize="iconSize"
+      :icon-size="iconSize"
+      :round-icon="roundIcon"
     >{{ content }}</gl-badge>
   `;
 
@@ -62,7 +63,7 @@ export const Variants = (args, { argTypes }) => ({
         :variant="variant"
         :size="size"
         :icon="icon"
-        :iconSize="iconSize"
+        :icon-size="iconSize"
         class="gl-mr-3"
       >{{ variant }}</gl-badge>
     </div>
@@ -86,7 +87,7 @@ export const Actionable = (args, { argTypes }) => ({
         :variant="variant"
         :size="size"
         :icon="icon"
-        :iconSize="iconSize"
+        :icon-size="iconSize"
         class="gl-mr-3"
       >{{ variant }}</gl-badge>
     </div>
@@ -114,7 +115,7 @@ export const Sizes = (args, { argTypes }) => ({
         :variant="variant"
         :size="size"
         :icon="icon"
-        :iconSize="iconSize"
+        :icon-size="iconSize"
         class="gl-mr-3"
       >{{ size }}</gl-badge>
     </div>
@@ -130,13 +131,19 @@ export const BadgeIcon = (args, { argTypes }) => ({
   components: { GlBadge },
   props: Object.keys(argTypes),
   template: `
-    <div class="gl-display-flex gl-gap-3">
-      <gl-badge variant="tier" icon="license">16px icon</gl-badge>
-      <gl-badge variant="tier" icon="license-sm" iconSize="sm">12px icon</gl-badge>
+    <div style="display: grid; grid-template-columns: repeat(4, 150px); row-gap: 8px; align-items: center;">
+      <div><gl-badge :variant="variant" size="sm" icon="terminal" icon-size="sm">12px default icon</gl-badge></div>
+      <div><gl-badge :variant="variant" icon="terminal" icon-size="sm">12px default icon</gl-badge></div>
+      <div><gl-badge :variant="variant" icon="terminal">16px default icon</gl-badge></div>
+      <div><gl-badge :variant="variant" size="lg" icon="terminal" icon-size="md">16px default icon</gl-badge></div>
+      <div><gl-badge :variant="variant" size="sm" icon="status" :round-icon="true" icon-size="sm">12px round icon</gl-badge></div>
+      <div><gl-badge :variant="variant" icon="status" :round-icon="true" icon-size="sm">12px round icon</gl-badge></div>
+      <div><gl-badge :variant="variant" icon="status" :round-icon="true">16px round icon</gl-badge></div>
+      <div><gl-badge :variant="variant" size="lg" icon="status" :round-icon="true" icon-size="md">16px round icon</gl-badge></div>
     </div>
   `,
 });
-BadgeIcon.argTypes = disableControls(['content', 'iconSize']);
+BadgeIcon.argTypes = disableControls(['content', 'iconSize', 'icon', 'size', 'roundIcon']);
 
 export const IconOnly = (args, { argTypes }) => ({
   components: { GlBadge },
@@ -148,7 +155,15 @@ export const IconOnly = (args, { argTypes }) => ({
         :variant="variant"
         :size="size"
         :icon="icon"
-        :iconSize="iconSize"
+        :icon-size="iconSize"
+      />
+      <gl-badge
+        :href="href"
+        :variant="variant"
+        :size="size"
+        :icon="icon"
+        :icon-size="iconSize"
+        :round-icon="true"
       />
     </div>
   `,
@@ -157,6 +172,7 @@ IconOnly.args = generateProps({
   variant: badgeVariantOptions.success,
   icon: 'calendar',
 });
+IconOnly.argTypes = disableControls(['content', 'roundIcon']);
 
 export const AllVariantsAndCategories = (args, { argTypes }) => ({
   components: { GlBadge },
@@ -164,32 +180,32 @@ export const AllVariantsAndCategories = (args, { argTypes }) => ({
   template: `
   <div>
     <div class="gl-display-flex gl-gap-3">
-      <gl-badge variant="info" size="sm" icon="terminal" iconSize="sm" />
-      <gl-badge variant="info" size="sm" icon="cancel" :roundIcon="true" iconSize="sm" />
-      <gl-badge variant="info" size="sm" icon="terminal" iconSize="sm">Small</gl-badge>
-      <gl-badge variant="info" size="sm" icon="cancel" :roundIcon="true" iconSize="sm">Small</gl-badge>
+      <gl-badge variant="info" size="sm" icon="terminal" icon-size="sm" />
+      <gl-badge variant="info" size="sm" icon="cancel" :round-icon="true" icon-size="sm" />
+      <gl-badge variant="info" size="sm" icon="terminal" icon-size="sm">Small</gl-badge>
+      <gl-badge variant="info" size="sm" icon="cancel" :round-icon="true" icon-size="sm">Small</gl-badge>
       <gl-badge variant="info" size="sm">Small</gl-badge>
       <gl-badge variant="info" size="sm">5</gl-badge>
     </div>
     <div class="gl-display-flex gl-gap-3 gl-mt-6">
-      <gl-badge variant="info" size="md" icon="terminal" iconSize="sm" />
-      <gl-badge variant="info" size="md" icon="cancel" :roundIcon="true" iconSize="sm" />
-      <gl-badge variant="info" size="md" icon="terminal" iconSize="sm">Medium</gl-badge>
-      <gl-badge variant="info" size="md" icon="cancel" :roundIcon="true" iconSize="sm">Medium</gl-badge>
+      <gl-badge variant="info" size="md" icon="terminal" icon-size="sm" />
+      <gl-badge variant="info" size="md" icon="cancel" :round-icon="true" icon-size="sm" />
+      <gl-badge variant="info" size="md" icon="terminal" icon-size="sm">Medium</gl-badge>
+      <gl-badge variant="info" size="md" icon="cancel" :round-icon="true" icon-size="sm">Medium</gl-badge>
       <gl-badge variant="info" size="md">Medium</gl-badge>
     </div>
     <div class="gl-display-flex gl-gap-3 gl-mt-3">
-      <gl-badge variant="info" size="md" icon="terminal" iconSize="md" />
-      <gl-badge variant="info" size="md" icon="cancel" :roundIcon="true" iconSize="md" />
-      <gl-badge variant="info" size="md" icon="terminal" iconSize="md">Medium</gl-badge>
-      <gl-badge variant="info" size="md" icon="cancel" :roundIcon="true" iconSize="md">Medium</gl-badge>
+      <gl-badge variant="info" size="md" icon="terminal" icon-size="md" />
+      <gl-badge variant="info" size="md" icon="cancel" :round-icon="true" icon-size="md" />
+      <gl-badge variant="info" size="md" icon="terminal" icon-size="md">Medium</gl-badge>
+      <gl-badge variant="info" size="md" icon="cancel" :round-icon="true" icon-size="md">Medium</gl-badge>
       <gl-badge variant="info" size="md">5</gl-badge>
     </div>
     <div class="gl-display-flex gl-gap-3 gl-mt-6">
-      <gl-badge variant="info" size="lg" icon="terminal" iconSize="md" />
-      <gl-badge variant="info" size="lg" icon="cancel" :roundIcon="true" iconSize="md" />
-      <gl-badge variant="info" size="lg" icon="terminal" iconSize="md">Large</gl-badge>
-      <gl-badge variant="info" size="lg" icon="cancel" :roundIcon="true" iconSize="md">Large</gl-badge>
+      <gl-badge variant="info" size="lg" icon="terminal" icon-size="md" />
+      <gl-badge variant="info" size="lg" icon="cancel" :round-icon="true" icon-size="md" />
+      <gl-badge variant="info" size="lg" icon="terminal" icon-size="md">Large</gl-badge>
+      <gl-badge variant="info" size="lg" icon="cancel" :round-icon="true" icon-size="md">Large</gl-badge>
       <gl-badge variant="info" size="lg">5</gl-badge>
       <gl-badge variant="info" size="lg">Large</gl-badge>
     </div>
