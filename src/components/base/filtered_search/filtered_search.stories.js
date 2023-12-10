6d75cf2a314aa1c0fc863587be4c2fd15586b6c9
Daniele Rossetti <drossetti@gitlab.com>
2023-12-10 22:36:54 +0000

    template cleanup

--- src/components/base/filtered_search/filtered_search.stories.js
+++ src/components/base/filtered_search/filtered_search.stories.js
@@ -370,45 +370,42 @@ const DateToken = {
   },
 
   template: `
-    <div>
-      <gl-filtered-search-token
-        :data-segment-input-attributes="dataSegmentInputAttributes"
-        v-bind="{ ...this.$props, ...this.$attrs }"
-        v-on="$listeners"
-        :view-only="showPicker"
-        :active="isActive"
-        :value="computedValue"
-        @complete="handleComplete"
-      >
-        <template #before-data-segment-input="{ submitValue }">
-          <gl-icon
-            v-if="showPicker" 
-            class="gl-text-gray-500"
-            name="calendar"
-            style="margin-right: -20px; z-index: 1; pointer-events: none;"
-          />
-          <!-- TODO reuse classes? -->
-          <div v-if="showPicker" 
-            class="gl-absolute gl-z-index-9999 gl-p-4 gl-bg-white gl-border-1 gl-border-gray-200 gl-mt-2 gl-mb-2 gl-p-0 gl-rounded-base gl-shadow-x0-y2-b4-s0" style="top: 100%" 
-            ref="datePicker"
-          >
-            <gl-daterange-picker @input="selectValue($event, submitValue)" /> 
-          </div> 
-        </template>
+    <gl-filtered-search-token
+      :data-segment-input-attributes="dataSegmentInputAttributes"
+      v-bind="{ ...this.$props, ...this.$attrs }"
+      v-on="$listeners"
+      :view-only="showPicker"
+      :active="isActive"
+      :value="computedValue"
+      @complete="handleComplete"
+    >
+      <template #before-data-segment-input="{ submitValue }" v-if="showPicker" >
+        <gl-icon
+          class="gl-text-gray-500"
+          name="calendar"
+          style="margin-right: -20px; z-index: 1; pointer-events: none;"
+        />
+        <!-- TODO reuse classes? -->
+        <div
+          class="gl-absolute gl-bg-white gl-border-1 gl-border-gray-200 gl-my-2 gl-p-4 gl-rounded-base gl-shadow-x0-y2-b4-s0" style="top: 100%" 
+          ref="datePicker"
+        >
+          <gl-daterange-picker @input="selectValue($event, submitValue)" /> 
+        </div> 
+      </template>
 
-        <template #suggestions>
-          <div  v-if="!showPicker">
-            <gl-filtered-search-suggestion
-              v-for="token in availableDefaultSuggestions"
-              :key="token.value"
-              :value="token.value"
-            >
-              {{ token.text }}
-            </gl-filtered-search-suggestion>
-          </div>
-        </template>
-      </gl-filtered-search-token>
-    </div>
+      <template #suggestions>
+        <div  v-if="!showPicker">
+          <gl-filtered-search-suggestion
+            v-for="token in availableDefaultSuggestions"
+            :key="token.value"
+            :value="token.value"
+          >
+            {{ token.text }}
+          </gl-filtered-search-suggestion>
+        </div>
+      </template>
+    </gl-filtered-search-token>
   `,
 };
 
