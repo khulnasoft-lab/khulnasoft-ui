e6de40f7569c30e1fa9dd52b035190dd8d6bfb35
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2023-12-12 19:26:49 -0500

    test(gltoken): add accessibility e2e tests

---
+++ cypress/e2e/token.cy.js
@@ -0,0 +1,19 @@
+describe('GlToken', () => {
+  function checkA11yDefaultState() {
+    cy.visitStory('base/gl-token--default');
+    cy.glCheckA11y();
+  }
+  function checkA11yWithViewOnly() {
+    cy.visitStory('base/gl-token--view-only');
+    cy.glCheckA11y();
+  }
+  function checkA11yWithAvatar() {
+    cy.visitStory('base/gl-token--with-avatar');
+    cy.glCheckA11y();
+  }
+  it('passes axe accessibility checks', () => {
+    checkA11yDefaultState();
+    checkA11yWithViewOnly();
+    checkA11yWithAvatar();
+  });
+});
