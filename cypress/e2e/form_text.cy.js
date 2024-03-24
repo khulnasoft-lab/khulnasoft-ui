f8a2a63e89a1839965fc7d619988459d32223d74
Drew Cauchi <drewcauchi@gmail.om>
2024-03-24 16:42:52 +0100

    test(GlFormText): Axe Accessibility Test for GlFormText
    Created e2e accessibility text for default form text

---
+++ cypress/e2e/form_text.cy.js
@@ -0,0 +1,12 @@
+describe('GlFormText', () => {
+  function checkA11YDefaultState() {
+    cy.visitStory('base/form/form-text');
+    cy.glCheckA11y();
+  }
+
+  it('passes axe accessibility audits', { tags: 'a11y' }, () => {
+    cy.glRunA11yTests({
+      checkA11YDefaultState,
+    });
+  });
+});
