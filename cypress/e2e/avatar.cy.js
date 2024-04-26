05291bd9a99f7e35d5a5c3b1f4d8d2781dc3d6bc
Deepika Guliani <dguliani@gitlab.com>
2024-04-26 13:23:35 +0530

    test(avatar): apply maintainer feedback

--- cypress/e2e/avatar.cy.js
+++ cypress/e2e/avatar.cy.js
@@ -32,20 +32,31 @@ describe('GlAvatarLabeled', () => {
 });
 
 describe('GlAvatarLink', () => {
-  function checkA11YDefaultState() {
-    cy.visitStory('base/avatar/avatar-link');
-  }
-
   function checkA11yWithLinks() {
     cy.visitStory('base/avatar/avatar-link', {
       story: 'with-labeled-avatar',
     });
   }
 
+  function checkA11yWithLinksHoverState() {
+    cy.visitStory('base/avatar/avatar-link', {
+      story: 'with-labeled-avatar',
+    });
+    cy.get('.gl-avatar-link').realHover();
+  }
+
+  function checkA11yWithLinksFocusState() {
+    cy.visitStory('base/avatar/avatar-link', {
+      story: 'with-labeled-avatar',
+    });
+    cy.get('.gl-avatar-link').focus();
+  }
+
   it('passes axe accessibility audits', { tags: '@a11y' }, () => {
     cy.glRunA11yTests({
-      checkA11YDefaultState,
       checkA11yWithLinks,
+      checkA11yWithLinksHoverState,
+      checkA11yWithLinksFocusState,
     });
   });
 });
@@ -61,10 +72,19 @@ describe('GlAvatarsInline', () => {
     cy.get('.gl-avatars-inline-child:first').realHover();
   }
 
+  function checkA11yAvatarsInlineFocusState() {
+    cy.visitStory('base/avatar/avatars-inline', {
+      story: 'with-links-and-tooltips',
+    });
+
+    cy.get('.gl-avatar-link:first').focus();
+  }
+
   it('passes axe accessibility audits', { tags: '@a11y' }, () => {
     cy.glRunA11yTests({
       checkA11YDefaultState,
       checkA11yAvatarsInlineHoverState,
+      checkA11yAvatarsInlineFocusState,
     });
   });
 });
