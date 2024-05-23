a962953e3b0d21b0359abb97f658596db89a58ba
Lukas Eipert <leipert@gitlab.com>
2024-05-23 09:43:34 +0200

    ci: Parallelize visual specs
    Because why not

--- bin/run-visual-tests.sh
+++ bin/run-visual-tests.sh
@@ -6,10 +6,15 @@ if [ "$CI" = true ]; then
   args="${1}"
   json="--json --outputFile tests/results.json"
 
+  if [ -n "$CI_NODE_INDEX" ]; then
+    args="${args} --shard $CI_NODE_INDEX/$CI_NODE_TOTAL"
+  fi
+
   if ! yarn test:visual:internal "${args} ${json}"; then
     echo "Some tests failed. Due to flakyness we are retrying them..."
     yarn test:visual:internal "${args}"
   fi
 else
   echo "Visual regression tests can only be run in CI at the moment."
+  exit 1
 fi
