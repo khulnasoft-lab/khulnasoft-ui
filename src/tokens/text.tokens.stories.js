1b7ab89584e803189da479f7ad8a60588d74aec5
sdejonge <sdejonge@gitlab.com>
2024-05-24 12:00:30 +1000

    refactor: simplify text design token stories
    Remove `text.dark.tokens.stories.js` add stories to Dark in
    `text.tokens.stories.js`

--- src/tokens/text.tokens.stories.js
+++ src/tokens/text.tokens.stories.js
@@ -1,4 +1,5 @@
 import COMPILED_TOKENS from './build/json/tokens.json';
+import COMPILED_TOKENS_DARK from './build/json/tokens.dark.json';
 import { createDesignTokenStory } from './common_story_options';
 
 export const Default = createDesignTokenStory({
@@ -11,7 +12,18 @@ export const Default = createDesignTokenStory({
   isBackgroundColorStory: false,
 });
 
+export const Dark = createDesignTokenStory({
+  containerClass: 'gl-bg-gray-950',
+  tokens: {
+    primary: COMPILED_TOKENS_DARK.text.primary,
+    secondary: COMPILED_TOKENS_DARK.text.secondary,
+    tertiary: COMPILED_TOKENS_DARK.text.tertiary,
+    ...COMPILED_TOKENS_DARK.text.color,
+  },
+  isBackgroundColorStory: false,
+});
+
 // eslint-disable-next-line storybook/csf-component
 export default {
-  title: 'tokens/text/light',
+  title: 'tokens/text',
 };
