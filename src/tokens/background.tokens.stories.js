3e1df35727c9651023c5cc50eff61f180d95b95c
sdejonge <sdejonge@gitlab.com>
2024-05-24 11:55:54 +1000

    chore: simplify storybook stories

--- src/tokens/background.tokens.stories.js
+++ src/tokens/background.tokens.stories.js
@@ -1,4 +1,5 @@
 import COMPILED_TOKENS from './build/json/tokens.json';
+import COMPILED_TOKENS_DARK from './build/json/tokens.dark.json';
 import { createDesignTokenStory } from './common_story_options';
 
 export const Default = createDesignTokenStory({
@@ -7,7 +8,13 @@ export const Default = createDesignTokenStory({
   },
 });
 
+export const Dark = createDesignTokenStory({
+  tokens: {
+    ...COMPILED_TOKENS_DARK.background.color,
+  },
+});
+
 // eslint-disable-next-line storybook/csf-component
 export default {
-  title: 'tokens/background/light',
+  title: 'tokens/background',
 };
