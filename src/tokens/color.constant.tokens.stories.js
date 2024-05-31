feab27d8b1e2f6f7a9dab9fb82aa92540822a1b6
sdejonge <sdejonge@gitlab.com>
2024-05-31 16:06:04 +1000

    feat(DesignTokens): add theme design tokens
    Add Storybook stories
    Generate design token output

--- src/tokens/color.constant.tokens.stories.js
+++ src/tokens/color.constant.tokens.stories.js
@@ -13,6 +13,24 @@ export const Red = createDesignTokenStory({ tokens: COMPILED_TOKENS.color.red })
 
 export const Purple = createDesignTokenStory({ tokens: COMPILED_TOKENS.color.purple });
 
+export const ThemeIndigo = createDesignTokenStory({
+  tokens: COMPILED_TOKENS.color['theme-indigo'],
+});
+
+export const ThemeBlue = createDesignTokenStory({ tokens: COMPILED_TOKENS.color['theme-blue'] });
+
+export const ThemeLightBlue = createDesignTokenStory({
+  tokens: COMPILED_TOKENS.color['theme-light-blue'],
+});
+
+export const ThemeGreen = createDesignTokenStory({ tokens: COMPILED_TOKENS.color['theme-green'] });
+
+export const ThemeRed = createDesignTokenStory({ tokens: COMPILED_TOKENS.color['theme-red'] });
+
+export const ThemeLightRed = createDesignTokenStory({
+  tokens: COMPILED_TOKENS.color['theme-light-red'],
+});
+
 // eslint-disable-next-line storybook/csf-component
 export default {
   title: 'tokens/color/constant',
