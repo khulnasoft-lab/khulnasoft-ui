12070583e28bef1a5dafc2fd504e92754f8337c3
Lukas Eipert <leipert@gitlab.com>
2024-07-03 22:18:38 +0200

    feat: Add utility class migration script
    This adds a migration script for legacy utilities to tailwind utilities.
    
    This was extracted from:
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/154442
    
    The idea is simple:
    1. We give a list of files via stdin or iterate over all source files in
       the current directory or in a given list of directories.
    2. We replace legacy utility class names with their tailwind equivalents
       (list maintained `bin/lib/tailwind_equivalents.js`). Note, not all
       legacy utils are mapped, but just the ones currently in use in the
       GitLab project and other important projects.
    3. You test that the changes have the expected results. Migration
       strategy for larger projects might be folder by folder or route by
       route.
    
    Co-authored-by: Mark Florian <mflorian@gitlab.com>
    Co-authored-by: Paul GV <pgascouvaillancourt@gitlab.com>

---
+++ bin/migrate_custom_utils_to_tw.mjs
@@ -0,0 +1,301 @@
+#!/usr/bin/env node
+/* eslint-disable no-continue, no-nested-ternary, no-await-in-loop */
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { readFile, writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import readline from 'node:readline/promises';
+import { format, resolveConfig } from 'prettier';
+import { sync as globbySync } from 'globby';
+import * as tailwindPlugin from 'prettier-plugin-tailwindcss';
+import { tailwindEquivalents } from './lib/tailwind_equivalents.js';
+
+function createRewriter(config, migrationsToDo) {
+  const { tailwindConfig, dryRun } = config;
+
+  return async function rewrite(file) {
+    const contents = await readFile(file, { encoding: 'utf8' });
+
+    let newContents = contents;
+
+    for (const { fromRegExp, to } of migrationsToDo) {
+      newContents = newContents.replaceAll(fromRegExp, to);
+    }
+
+    if (contents === newContents) {
+      console.warn(`No changes to ${file}`);
+      return;
+    }
+
+    if (dryRun) {
+      console.warn(`Would fix up ${file}`);
+      return;
+    }
+
+    if (file.endsWith('.js') || file.endsWith('.vue')) {
+      const prettierConfig = (await resolveConfig(file)) || {};
+      newContents = await format(newContents, {
+        filepath: file,
+        ...prettierConfig,
+        plugins: [tailwindPlugin],
+        tailwindConfig,
+      });
+    }
+    await writeFile(file, newContents, { encoding: 'utf8' });
+    console.warn(`Fixed ${file}`);
+  };
+}
+
+function validateMigrations(processedMigrations) {
+  const errors = [];
+
+  for (const { from, to } of processedMigrations) {
+    if (from.endsWith('!') !== /!-?gl/.test(to)) {
+      errors.push(`Inconsistent importance: ${from}, ${to}`);
+    }
+  }
+
+  // Make sure there are no duplicates
+  const froms = new Set();
+  const tos = new Set();
+  for (const { from, to } of processedMigrations) {
+    froms.add(from);
+    tos.add(to);
+  }
+
+  // Make sure important class exists for each non-important class.
+  // It doesn't matter if the classes don't exist, it's fast enough to check
+  // for them anyway.
+  for (const { from } of processedMigrations) {
+    if (from.endsWith('!')) continue;
+
+    if (!froms.has(`${from}!`)) {
+      errors.push(`Missing important class for ${from}`);
+    }
+  }
+
+  // Make sure responsive breakpoints are consistent
+  for (const { from, to } of processedMigrations) {
+    for (const bp of ['xs', 'sm', 'md', 'lg', 'xl']) {
+      if (from.includes(`-${bp}-`) !== to.startsWith(`${bp}:`)) {
+        errors.push(`Inconsistent breakpoints: ${from}, ${to}`);
+      }
+    }
+  }
+
+  // Make sure no legacy classes are substrings Tailwind classes, which could
+  // cause replacement errors. This *shouldn't* occur, but might as well check
+  // for it anyway. For instance:
+  //   1. Replace legacy class "gl-foo-bar" with Tailwind class "gl-qux-baz"
+  //   2. Replace legacy class "gl-qux" with Tailwind class "gl-qux-foo"
+  //   3. The Tailwind class "gl-qux-baz" is now borked to "gl-qux-foo-baz"
+  for (const from of froms) {
+    for (const to of tos) {
+      if (to.includes(from)) {
+        errors.push(`Tailwind class "${to}" contains legacy class "${from}" as substring`);
+      }
+    }
+  }
+
+  // Double-check we sorted classes correctly
+  processedMigrations.forEach(({ from }, i, arr) => {
+    const nextFrom = arr[i + 1]?.from;
+    if (from.length < nextFrom?.length ?? -Infinity) {
+      errors.push(`Incorrect ordering: ${from} followed by ${nextFrom}`);
+    }
+  });
+
+  if (errors.length > 0) {
+    console.error([`Invalid migrations (${errors.length})\n`, ...errors].join('\n'));
+    return false;
+  }
+
+  return true;
+}
+
+function legacyClassToImportant(klass) {
+  return klass.endsWith('!') ? klass : `${klass}!`;
+}
+
+function tailwindClassToImportant(klass) {
+  return klass.includes('!gl-') ? klass : klass.replace(/(^|:)(-?gl-)/, '$1!$2');
+}
+
+function filterOutNonStringValues(rawMigrations) {
+  return Object.entries(rawMigrations).filter(([, to]) => typeof to === 'string');
+}
+
+function addImportantVariants(rawMigrations) {
+  const map = rawMigrations.reduce((acc, [from, to]) => {
+    acc.set(from, to);
+
+    const importantFrom = legacyClassToImportant(from);
+
+    if (!Object.hasOwn(rawMigrations, importantFrom)) {
+      acc.set(importantFrom, tailwindClassToImportant(to));
+    }
+
+    return acc;
+  }, new Map());
+  return Array.from(map.entries()).map(([from, to]) => ({ from, to }));
+}
+
+function addFromRegExps(rawMigrations) {
+  const classChars = ['-', '\\w', '!', ':'].join('|');
+  return rawMigrations.map((migration) => ({
+    ...migration,
+    fromRegExp: new RegExp(`(?<!${classChars})${migration.from}(?!${classChars})`, 'g'),
+  }));
+}
+
+function sortMigrations(unsortedMigrations) {
+  return (
+    unsortedMigrations
+      .slice()
+      // Migrate "foobar" and "bar foo" before "foo" so we don't incorrectly
+      // migrate "foo".
+      .sort((a, b) => {
+        if (a.from.length < b.from.length) return 1;
+        if (a.from.length > b.from.length) return -1;
+        return 0;
+      })
+  );
+}
+
+async function parseMigrations(obj) {
+  try {
+    return sortMigrations(addFromRegExps(addImportantVariants(filterOutNonStringValues(obj))));
+  } catch (error) {
+    console.error(error.message);
+    return [];
+  }
+}
+
+async function getFilesAndDirectories(directories, dryRun) {
+  // prettier-ignore
+  const directoriesPattern = directories.length === 0 ?
+    process.cwd()
+    : directories.length === 1 ?
+      directories[0]
+      : `{${directories.join(',')}}`;
+
+  const extensions = ['haml', 'rb', 'vue', 'js', 'snap', 'html'];
+  const filesPattern = `${directoriesPattern}/**/*.{${extensions.join(',')}}`;
+
+  return {
+    pattern: filesPattern,
+    directories: dryRun
+      ? globbySync(directoriesPattern, { onlyDirectories: true, gitignore: true })
+      : [],
+    files: globbySync(filesPattern, { onlyFiles: true, gitignore: true }),
+  };
+}
+
+function getArgsParser() {
+  const NO_EQUIVALENTS = Symbol('no-equivalents');
+
+  return yargs(hideBin(process.argv))
+    .usage(
+      'Usage: $0 [--migrations <path>] [--tailwind-config <path>] [--directory <path...>] [--from-stdin] [--dry-run]'
+    )
+    .option('directories', {
+      alias: 'd',
+      array: true,
+      describe: 'Space separated path segments.',
+      default: [process.cwd()],
+    })
+    .option('tailwind-config', {
+      alias: 't',
+      type: 'string',
+      describe: 'Optional. Path to tailwind config. Defaulting to GitLab UI default',
+      default: fileURLToPath(new URL('../tailwind.defaults.js', import.meta.url)),
+    })
+    .option('migrations', {
+      alias: 'm',
+      type: 'string',
+      describe:
+        'Optional. Path to migrations JSON file, with { oldClass: newClass, ... } structure',
+      default: NO_EQUIVALENTS,
+      defaultDescription: 'Defaults to definitions from @gitlab/ui',
+      coerce: async (value) => {
+        if (value === NO_EQUIVALENTS) {
+          return tailwindEquivalents;
+        }
+        return JSON.parse(await readFile(value, 'utf-8'));
+      },
+    })
+    .option('validate-migrations', {
+      describe: "Don't run validations against migrations",
+      type: 'boolean',
+      default: true,
+    })
+    .option('dry-run', {
+      describe: "Don't actually update files, but print more info",
+      type: 'boolean',
+      default: false,
+    })
+    .option('from-stdin', {
+      describe: "Don't actually update files, but print more info",
+      type: 'boolean',
+      default: false,
+    })
+    .help('help');
+}
+
+async function main() {
+  const argsParser = getArgsParser();
+
+  const program = await argsParser.parseAsync();
+
+  console.warn(program);
+
+  const migrations = await parseMigrations(program.migrations);
+
+  if (migrations.length === 0) {
+    console.warn('No migrations provided. Defaulting to built-in migrations');
+    argsParser.showHelp();
+    return;
+  }
+
+  if (program.validateMigrations && !validateMigrations(migrations)) return;
+
+  if (program.dryRun) {
+    console.warn('Will do approximately %d class migrations:', migrations.length);
+    console.warn(migrations.map((m) => `\t${m.from} => ${m.to}`).join('\n'));
+  }
+
+  const rewrite = createRewriter(program, migrations);
+
+  if (program.fromStdin) {
+    console.warn('Reading files from stdin:');
+    for await (const file of readline.createInterface({ input: process.stdin })) {
+      if (file.trim()) {
+        await rewrite(file);
+      }
+    }
+    return;
+  }
+
+  const { pattern, directories, files } = await getFilesAndDirectories(
+    program.directories ?? [],
+    program.dryRun
+  );
+
+  if (program.dryRun) {
+    console.warn(
+      [`Running on %d files across %d directories`, `(using pattern: %s).`].join('\n'),
+      files.length,
+      directories.length,
+      pattern
+    );
+
+    console.warn('Directories searched:');
+    console.warn(`\t${directories.join('\n\t')}`);
+  }
+
+  for (const file of files) {
+    await rewrite(file);
+  }
+}
+
+main();
