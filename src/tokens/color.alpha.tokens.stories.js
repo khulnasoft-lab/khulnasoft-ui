382de2f43b4f4a887c5d7bdebeeb581771093fda
sdejonge <sdejonge@gitlab.com>
2024-07-12 11:18:02 +1000

    chore: Apply alpha colors to button tertiary design tokens
    Add blue and red alpha colors

--- src/tokens/color.alpha.tokens.stories.js
+++ src/tokens/color.alpha.tokens.stories.js
@@ -8,6 +8,10 @@ export const Light = createDesignTokenStory({
   containerClass: 'gl-bg-gray-900 gl-text-white',
 });
 
+export const Blue = createDesignTokenStory({ tokens: COMPILED_TOKENS.color.alpha.blue });
+
+export const Red = createDesignTokenStory({ tokens: COMPILED_TOKENS.color.alpha.red });
+
 // eslint-disable-next-line storybook/csf-component
 export default {
   title: 'tokens/color/alpha',
