ea2b753b70174fc58d6ec3e5ae0580320d5fa86e
Vanessa Otto <votto@gitlab.com>
2024-08-08 12:29:07 +0200

    refactor(GlFormText): Replace small HTML tag with Util CSS classes

--- src/components/base/form/form_text/form_text.vue
+++ src/components/base/form/form_text/form_text.vue
@@ -7,17 +7,12 @@ export default {
       required: false,
       default: false,
     },
-    tag: {
-      type: String,
-      required: false,
-      default: 'small',
-    },
   },
 };
 </script>
 
 <template>
-  <component :is="tag" :class="{ 'gl-form-text-block': !inline }" class="gl-form-text">
+  <span :class="{ 'gl-form-text-block': !inline }" class="gl-form-text">
     <slot></slot>
-  </component>
+  </span>
 </template>
