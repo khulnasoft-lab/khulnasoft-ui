3ffb81959b9502bc100782f8e8df101dc74f7eb6
michaelangeloio <arivera@gitlab.com>
2024-08-18 21:03:21 -0400

    start breaking up components

---
+++ src/components/experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_menu/duo_chat_context_item_menu.stories.js
@@ -0,0 +1,66 @@
+import Vue from 'vue';
+import { action } from '@storybook/addon-actions';
+import GlDuoChatContextItemMenu from './duo_chat_context_item_menu.vue';
+
+// Create a global event bus
+const eventBus = new Vue();
+
+export default {
+  title: 'experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_menu',
+  component: GlDuoChatContextItemMenu,
+  argTypes: {
+    cursorPosition: { control: { type: 'number', min: 0, max: 1000, step: 10 } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { GlDuoChatContextItemMenu },
+  data() {
+    console.log('angelo testing', eventBus)
+    return {
+      localEventBus: eventBus, // Use the global event bus
+    };
+  },
+  mounted() {
+    this.localEventBus.$on('context_item_search_query', this.handleSearch);
+    this.localEventBus.$on('context_item_added', this.handleItemAdded);
+  },
+  beforeDestroy() {
+    this.localEventBus.$off('context_item_search_query', this.handleSearch);
+    this.localEventBus.$off('context_item_added', this.handleItemAdded);
+  },
+  methods: {
+    handleSearch({ category, query }) {
+      action('Search initiated')(category, query);
+      const mockResults = [
+        { id: '1', name: 'Item 1', isEnabled: true, info: { iid: 1 }, type: category },
+        { id: '2', name: 'Item 2', isEnabled: false, info: { iid: 2 }, type: category },
+        { id: '3', name: 'Item 3', isEnabled: true, info: { iid: 3 }, type: category },
+      ];
+      setTimeout(() => {
+        this.localEventBus.$emit('context_item_search_result', mockResults);
+      }, 300);
+    },
+    handleItemAdded(item) {
+      action('Item added')(item);
+    },
+    toggleMenu() {
+      this.localEventBus.$emit('toggle_context_menu', true);
+    },
+  },
+  template: `
+    <div>
+      <button @click="toggleMenu">Toggle Context Menu</button>
+      <gl-duo-chat-context-item-menu
+        :event-bus="localEventBus"
+        :cursor-position="cursorPosition"
+      />
+    </div>
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  cursorPosition: 100,
+};
---
+++ src/components/experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_menu/duo_chat_context_item_menu.vue
@@ -0,0 +1,256 @@
+<script>
+import debounce from 'lodash/debounce';
+import GlDropdownItem from '../../../../../../base/dropdown/dropdown_item.vue';
+import GlFormInput from '../../../../../../base/form/form_input/form_input.vue';
+import GlCard from '../../../../../../base/card/card.vue';
+import GlIcon from '../../../../../../base/icon/icon.vue';
+import GlDuoChatContextItemPopover from '../duo_chat_context_item_popover/duo_chat_context_item_popover.vue';
+
+export default {
+  name: 'GlDuoChatContextItemMenu',
+  components: {
+    GlDropdownItem,
+    GlFormInput,
+    GlCard,
+    GlIcon,
+    GlDuoChatContextItemPopover,
+  },
+  props: {
+    eventBus: {
+      type: Object,
+      required: true,
+    },
+    cursorPosition: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      categories: [
+        { label: 'Files', value: 'file', icon: 'document' },
+        { label: 'Issues', value: 'issue', icon: 'issues' },
+        { label: 'Merge Requests', value: 'merge_request', icon: 'merge-request' },
+      ],
+      selectedCategory: null,
+      searchQuery: '',
+      filteredItems: [],
+      activeIndex: 0,
+      showContextItemDropdown: false,
+    };
+  },
+  computed: {
+    showCategorySelection() {
+      return this.showContextItemDropdown && !this.selectedCategory;
+    },
+    showItemSearch() {
+      return this.showContextItemDropdown && this.selectedCategory;
+    },
+    currentItems() {
+      return this.showCategorySelection ? this.categories : this.filteredItems;
+    },
+  },
+  watch: {
+    showContextItemDropdown(newVal) {
+      if (newVal) {
+        this.activeIndex = 0;
+        this.$nextTick(() => {
+          this.focusSearchInput();
+        });
+      }
+    },
+    selectedCategory() {
+      this.activeIndex = 0;
+      this.$nextTick(() => {
+        this.focusSearchInput();
+      });
+    },
+    filteredItems() {
+      this.activeIndex = 0;
+    },
+  },
+  created() {
+    this.eventBus.$on('toggle_context_menu', this.toggleContextMenu);
+    this.eventBus.$on('context_item_search_result', this.handleSearchResult);
+  },
+  beforeDestroy() {
+    this.eventBus.$off('toggle_context_menu', this.toggleContextMenu);
+    this.eventBus.$off('context_item_search_result', this.handleSearchResult);
+  },
+  methods: {
+    toggleContextMenu(show) {
+      this.showContextItemDropdown = show;
+      if (show) {
+        this.activeIndex = 0;
+        this.$nextTick(() => {
+          this.focusSearchInput();
+        });
+      }
+    },
+    focusSearchInput() {
+      if (this.showItemSearch && this.$refs.searchInput && this.$refs.searchInput.$el) {
+        this.$refs.searchInput.$el.focus();
+      }
+    },
+    selectCategory(category) {
+      this.selectedCategory = category;
+      this.searchQuery = '';
+      this.filteredItems = [];
+      this.eventBus.$emit('context_item_search_query', { category: category.value, query: '' });
+    },
+    debouncedSearch: debounce(function () {
+      this.eventBus.$emit('context_item_search_query', {
+        category: this.selectedCategory.value,
+        query: this.searchQuery,
+      });
+    }, 300),
+    handleSearchResult(results) {
+      this.filteredItems = results;
+    },
+    selectItem(item) {
+      this.eventBus.$emit('context_item_added', { ...item, category: this.selectedCategory?.value });
+      this.resetSelection();
+    },
+    resetSelection() {
+      this.selectedCategory = null;
+      this.searchQuery = '';
+      this.filteredItems = [];
+      this.activeIndex = 0;
+      this.showContextItemDropdown = false;
+    },
+    handleKeydown(e) {
+      if (!this.showContextItemDropdown) return;
+
+      if (this.showItemSearch && !['ArrowDown', 'ArrowUp', 'Enter', 'Escape'].includes(e.key)) {
+        this.focusSearchInput();
+        return;
+      }
+
+      switch (e.key) {
+        case 'ArrowDown':
+          e.preventDefault();
+          this.activeIndex = (this.activeIndex + 1) % this.currentItems.length;
+          break;
+        case 'ArrowUp':
+          e.preventDefault();
+          this.activeIndex =
+            (this.activeIndex - 1 + this.currentItems.length) % this.currentItems.length;
+          break;
+        case 'Enter':
+          e.preventDefault();
+          if (!this.currentItems.length) {
+            return;
+          }
+          if (this.showCategorySelection) {
+            this.selectCategory(this.currentItems[this.activeIndex]);
+          } else {
+            this.selectItem(this.currentItems[this.activeIndex]);
+          }
+          break;
+        case 'Escape':
+          e.preventDefault();
+          this.eventBus.$emit('toggle_context_menu', false);
+          break;
+        default:
+          break;
+      }
+    },
+    onSearchInputKeydown(e) {
+      if (['ArrowDown', 'ArrowUp', 'Enter', 'Escape'].includes(e.key)) {
+        this.handleKeydown(e);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="gl-duo-chat-context-item gl-relative">
+    <gl-card v-if="showContextItemDropdown" class="context-item-card gl-position-absolute">
+      <template v-if="showCategorySelection">
+        <ul class="list-unstyled gl-mb-0">
+          <li v-for="(category, index) in categories" :key="category.value">
+            <gl-dropdown-item
+              :class="{ 'gl-bg-gray-50': index === activeIndex }"
+              @click="selectCategory(category)"
+            >
+              <div class="gl-display-flex gl-align-items-center">
+                <gl-icon :name="category.icon" class="gl-mr-2" />
+                {{ category.label }}
+              </div>
+            </gl-dropdown-item>
+          </li>
+        </ul>
+      </template>
+      <template v-else-if="showItemSearch">
+        <gl-form-input
+          ref="searchInput"
+          v-model="searchQuery"
+          :placeholder="`Search ${selectedCategory.label.toLowerCase()}...`"
+          class="gl-mb-3"
+          @input="debouncedSearch"
+          @keydown="onSearchInputKeydown"
+        />
+        <ul class="list-unstyled gl-mb-1">
+          <li v-for="(item, index) in filteredItems" :key="item.id">
+            <gl-dropdown-item
+              :id="`dropdown-item-${index}`"
+              :class="[
+                { 'gl-bg-gray-50': index === activeIndex },
+                { 'disabled-item': !item.isEnabled },
+              ]"
+              @click="item.isEnabled && selectItem(item)"
+            >
+              <div class="gl-display-flex gl-align-items-center gl-truncate">
+                <gl-icon
+                  :name="selectedCategory.icon"
+                  class="gl-mr-2"
+                  :class="{ 'gl-text-gray-500': !item.isEnabled }"
+                />
+                <span :class="{ 'gl-text-gray-500': !item.isEnabled }">{{ item.name }}</span>
+                <span class="gl-ml-3 gl-text-gray-300">
+                  <template v-if="item.type === 'file'">{{ item.info.relFilePath }}</template>
+                  <template v-else-if="item.type === 'merge_request'"
+                    >!{{ item.info.iid }}</template
+                  >
+                  <template v-else-if="item.type === 'issue'">#{{ item.info.iid }}</template>
+                </span>
+              </div>
+            </gl-dropdown-item>
+            <gl-duo-chat-context-item-popover
+              :item="item"
+              :target="`dropdown-item-${index}`"
+              placement="top"
+            />
+          </li>
+        </ul>
+      </template>
+    </gl-card>
+  </div>
+</template>
+
+<style scoped>
+.gl-duo-chat-context-item {
+  display: inline-block;
+}
+
+.context-item-card {
+  top: 100%;
+  left: v-bind('cursorPosition + "px"');
+  z-index: 100;
+  min-width: 250px;
+  max-width: 400px;
+  max-height: 300px;
+  overflow-y: auto;
+}
+
+.disabled-item {
+  cursor: not-allowed;
+  background-color: #f9f9f9;
+}
+
+.disabled-item:hover {
+  background-color: #f9f9f9 !important;
+}
+</style>
