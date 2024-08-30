a218651603df75f4568fb1ffdee9d1729ff50450
Peter Hegman <phegman@gitlab.com>
2024-08-30 11:53:48 -0700

    fix(GlFormInput): Set `noWheel` to `true` for number inputs
    To prevent blur bug on normal inputs

--- src/components/base/form/form_input/form_input.spec.js
+++ src/components/base/form/form_input/form_input.spec.js
@@ -1,5 +1,6 @@
 import { mount, shallowMount } from '@vue/test-utils';
 import { formInputWidths } from '../../../../utils/constants';
+import { BFormInput } from '../../../../vendor/bootstrap-vue/src/components/form-input/form-input';
 import GlFormInput from './form_input.vue';
 
 const modelEvent = GlFormInput.model.event;
@@ -8,9 +9,10 @@ const newValue = 'foo';
 describe('GlFormInput', () => {
   let wrapper;
 
-  const createComponent = (propsData = {}, mountFn = shallowMount) => {
+  const createComponent = ({ propsData = {}, mountFn = shallowMount, attrs = {} } = {}) => {
     wrapper = mountFn(GlFormInput, {
       propsData,
+      attrs,
     });
   };
 
@@ -20,7 +22,7 @@ describe('GlFormInput', () => {
       const widths = Object.values(formInputWidths).filter(Boolean);
 
       it.each(widths)('adds correct class for width %s', (width) => {
-        createComponent({ width });
+        createComponent({ propsData: { width } });
 
         expect(wrapper.classes()).toEqual(['gl-form-input', `gl-form-input-${width}`]);
       });
@@ -32,7 +34,7 @@ describe('GlFormInput', () => {
       });
 
       it('does not add a width class if passed null', () => {
-        createComponent({ width: null });
+        createComponent({ propsData: { width: null } });
 
         expect(wrapper.classes()).toEqual(['gl-form-input']);
       });
@@ -41,7 +43,7 @@ describe('GlFormInput', () => {
     describe('when object is passed', () => {
       describe('when `default` key is provided', () => {
         it('adds responsive CSS classes and base class', () => {
-          createComponent({ width: { default: 'md', md: 'lg', lg: 'xl' } });
+          createComponent({ propsData: { width: { default: 'md', md: 'lg', lg: 'xl' } } });
 
           expect(wrapper.classes()).toEqual([
             'gl-form-input',
@@ -54,7 +56,7 @@ describe('GlFormInput', () => {
 
       describe('when `default` key is not provided', () => {
         it('adds responsive CSS classes', () => {
-          createComponent({ width: { md: 'lg', lg: 'xl' } });
+          createComponent({ propsData: { width: { md: 'lg', lg: 'xl' } } });
 
           expect(wrapper.classes()).toEqual([
             'gl-form-input',
@@ -68,7 +70,7 @@ describe('GlFormInput', () => {
 
   describe('v-model', () => {
     beforeEach(() => {
-      createComponent({}, mount);
+      createComponent({ mountFn: mount });
 
       wrapper.setValue(newValue);
     });
@@ -87,7 +89,7 @@ describe('GlFormInput', () => {
       beforeEach(() => {
         jest.useFakeTimers();
 
-        createComponent({ debounce }, mount);
+        createComponent({ propsData: { debounce }, mountFn: mount });
 
         wrapper.setValue(newValue);
       });
@@ -110,7 +112,7 @@ describe('GlFormInput', () => {
 
   describe('lazy', () => {
     beforeEach(() => {
-      createComponent({ lazy: true }, mount);
+      createComponent({ propsData: { lazy: true }, mountFn: mount });
 
       wrapper.setValue(newValue);
     });
@@ -125,6 +127,26 @@ describe('GlFormInput', () => {
       wrapper.trigger(event);
 
       expect(wrapper.emitted(modelEvent)).toEqual([[newValue]]);
+    });
+  });
+
+  describe('when input type is `number`', () => {
+    beforeEach(() => {
+      createComponent({ attrs: { type: 'number' } });
+    });
+
+    it('sets `noWheel` prop to `true`', () => {
+      expect(wrapper.findComponent(BFormInput).props('noWheel')).toBe(true);
+    });
+  });
+
+  describe('when input type is not `number`', () => {
+    beforeEach(() => {
+      createComponent({ attrs: { type: 'text' } });
+    });
+
+    it('sets `noWheel` prop to `false`', () => {
+      expect(wrapper.findComponent(BFormInput).props('noWheel')).toBe(false);
     });
   });
 });
