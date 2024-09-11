a1dff063e910fc99365b03ce4e1fbfab6b575d65
Lukas Eipert <leipert@gitlab.com>
2024-09-11 13:29:19 +0200

    chore: re-add stories to tailwind
    Given that this for storybook mainly, we need to scan the stories as
    well.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
   presets: [defaults],
   content: [
     './src/**/*.{vue,js}',
-    '!./src/**/*.stories.js',
     '!./src/**/*.spec.js',
     '!./src/vendor/bootstrap-vue/**',
     './src/vendor/bootstrap-vue/src/**/*.{js,vue}',
