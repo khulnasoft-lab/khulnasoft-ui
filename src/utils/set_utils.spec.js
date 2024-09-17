fd239fd5b3813ac2de7a92eb9f455f66020495b3
Artur Fedorov <afedorov@gitlab.com>
2024-09-17 23:27:13 +0200

    chore: applied maintainer changes

--- src/utils/set_utils.spec.js
+++ src/utils/set_utils.spec.js
@@ -11,7 +11,7 @@ describe('set utils', () => {
     ${{}}               | ${'key'}   | ${'value'}  | ${{ key: 'value' }}
     ${{ key: 'value' }} | ${['key']} | ${'value'}  | ${{ key: 'value' }}
     ${{ key: 'value' }} | ${null}    | ${'value'}  | ${{}}
-    ${{ key: 'value' }} | ${'key1'}  | ${'value'}  | ${{ key: 'value', key1: 'value' }}
+    ${{ key: 'value' }} | ${'key1'}  | ${'value1'} | ${{ key: 'value', key1: 'value1' }}
     ${{ key: 'value' }} | ${'key'}   | ${'value1'} | ${{ key: 'value1' }}
     ${[]}               | ${'key'}   | ${'value1'} | ${{ key: 'value1' }}
   `('set new property on object or update existing', ({ source, key, value, output }) => {
