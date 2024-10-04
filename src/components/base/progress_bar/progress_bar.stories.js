joolstorrentecalo/gitlab-ui

feat(GlProgressBar): Remove some progress bar color variants
BREAKING CHANGE:

* Rename variantOptions to progressBarOptions since they're only used
for the GlProgressBar
* Remove options: secondary, light, dark, info

src/components/base/progress_bar/progress_bar.stories.js
@@ -1,7 +1,7 @@
-import { variantOptions } from '../../../utils/constants';
+import { progressBarOptions } from '../../../utils/constants';
 import GlProgressBar from './progress_bar.vue';
 
-const generateProps = ({ value = 30, variant = variantOptions.primary } = {}) => ({
+const generateProps = ({ value = 30, variant = progressBarOptions.primary } = {}) => ({
   value,
   variant,
 });
@@ -16,7 +16,7 @@ export const Default = Template.bind({});
 Default.args = generateProps();
 
 export const SuccessVariant = Template.bind({});
-SuccessVariant.args = generateProps({ variant: variantOptions.success });
+SuccessVariant.args = generateProps({ variant: progressBarOptions.success });
 SuccessVariant.parameters = { controls: { disabled: true } };
 
 export default {
@@ -27,7 +27,7 @@ export default {
   },
   argTypes: {
     variant: {
-      options: variantOptions,
+      options: progressBarOptions,
       control: 'select',
     },
   },

src/utils/constants.js
@@ -29,15 +29,11 @@ export const HEX_REGEX = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
 
 export const LEFT_MOUSE_BUTTON = 0;
 
-export const variantOptions = {
+export const progressBarOptions = {
   primary: 'primary',
-  secondary: 'secondary',
   success: 'success',
   warning: 'warning',
   danger: 'danger',
-  info: 'info',
-  light: 'light',
-  dark: 'dark',
 };
 
 export const badgeSizeOptions = {

khulnasoft-lab/khulnasoft-ui

src/components/base/progress_bar/progress_bar.stories.js
@@ -1,7 +1,7 @@
-import { variantOptions } from '../../../utils/constants';
+import { progressBarVariantOptions } from '../../../utils/constants';
 import GlProgressBar from './progress_bar.vue';
 
-const generateProps = ({ value = 30, variant = variantOptions.primary } = {}) => ({
+const generateProps = ({ value = 30, variant = progressBarVariantOptions.primary } = {}) => ({
   value,
   variant,
 });
@@ -16,7 +16,7 @@ export const Default = Template.bind({});
 Default.args = generateProps();
 
 export const SuccessVariant = Template.bind({});
-SuccessVariant.args = generateProps({ variant: variantOptions.success });
+SuccessVariant.args = generateProps({ variant: progressBarVariantOptions.success });
 SuccessVariant.parameters = { controls: { disabled: true } };
 
 export default {
@@ -27,7 +27,7 @@ export default {
   },
   argTypes: {
     variant: {
-      options: variantOptions,
+      options: progressBarVariantOptions,
       control: 'select',
     },
   },

src/utils/constants.js
@@ -29,15 +29,11 @@ export const HEX_REGEX = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
 
 export const LEFT_MOUSE_BUTTON = 0;
 
-export const variantOptions = {
+export const progressBarVariantOptions = {
   primary: 'primary',
-  secondary: 'secondary',
   success: 'success',
   warning: 'warning',
   danger: 'danger',
-  info: 'info',
-  light: 'light',
-  dark: 'dark',
 };
 
 export const badgeSizeOptions = {