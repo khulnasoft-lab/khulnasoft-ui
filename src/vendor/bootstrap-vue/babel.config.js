98a1d33bc73831fbfd6dec8c6dce6b0dc8f667cf
Lukas Eipert <leipert@gitlab.com>
2024-10-14 13:52:01 +0200

    chore(bootstrap-vue): Remove JS build dependencies
    We are consuming the bootstrap-vue code from source, so we can drop most
    babel, rollup and terser related dependencies.

--- src/vendor/bootstrap-vue/babel.config.js
+++ src/vendor/bootstrap-vue/babel.config.js
@@ -1,23 +1,9 @@
 module.exports = api => {
-  const isDocs = api.env('docs')
-
-  const presets = []
-  if (!isDocs) {
-    presets.push(['@babel/env', { useBuiltIns: 'entry', corejs: { version: 3 } }])
-  }
+  api.cache(true)
 
   return {
-    presets,
-    env: {
-      es: {
-        plugins: [['@babel/plugin-transform-modules-commonjs', { loose: true }]]
-      },
-      esm: {
-        presets: [['@babel/env', { modules: false }]]
-      },
-      test: {
-        presets: [['@babel/env', { targets: { node: 'current' } }]]
-      }
-    }
+    presets: [
+      ['@babel/env', { useBuiltIns: 'entry', corejs: { version: 3 }, targets: { node: 'current' } }]
+    ]
   }
 }
