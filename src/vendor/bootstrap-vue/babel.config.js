b35932f39fca6a850b3086e2e209cd6c479b74bb
Lukas Eipert <leipert@gitlab.com>
2024-10-17 10:14:30 +0200

    chore(bootstrap-vue): Remove testing related dependencies
    Given that we are now testing on the top-level @gitlab/ui project, we
    can get rid of these dependencies. Woop. Woop.

--- src/vendor/bootstrap-vue/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = api => {
-  api.cache(true)
-
-  return {
-    presets: [
-      ['@babel/env', { useBuiltIns: 'entry', corejs: { version: 3 }, targets: { node: 'current' } }]
-    ]
-  }
-}
