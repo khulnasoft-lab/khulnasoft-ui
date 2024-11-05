61a329ee9a8fa38018fa9eedd36a5467aaff90b3
Vanessa Otto <votto@gitlab.com>
2024-11-05 10:21:19 +0100

    chore(BootstrapVue): Remove installer code

--- src/vendor/bootstrap-vue/src/index.js
+++ src/vendor/bootstrap-vue/src/index.js
@@ -1,31 +1,14 @@
-import { installFactory } from './utils/plugins'
-import { componentsPlugin } from './components'
 import { BVConfigPlugin } from './bv-config'
 
 const NAME = 'BootstrapVue'
 
-// --- BootstrapVue installer ---
-const install = /*#__PURE__*/ installFactory({
-  plugins: {
-    componentsPlugin
-  }
-})
-
 // --- BootstrapVue plugin ---
 const BootstrapVue = /*#__PURE__*/ {
-  install,
   NAME
 }
 
 // --- Named exports for BvConfigPlugin ---
 export {
-  // Installer exported in case the consumer does not import `default`
-  // as the plugin in CommonJS build (or does not have interop enabled for CommonJS)
-  // Both the following will work:
-  //   BootstrapVue = require('bootstrap-vue')
-  //   BootstrapVue = require('bootstrap-vue').default
-  //   Vue.use(BootstrapVue)
-  install,
   NAME,
   // BootstrapVue config plugin
   BVConfigPlugin,
