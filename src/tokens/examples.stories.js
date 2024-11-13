6fa440591be9fc79a0547267e43c02daaa6753d8
Jeremy Elder <jelder@gitlab.com>
2024-11-13 16:28:20 -0600

    feat(Borders): Update border tokens
    Update border design tokens and add examples and decouple
    the dashed button from the strong border.

--- src/tokens/examples.stories.js
+++ src/tokens/examples.stories.js
@@ -23,6 +23,68 @@ export const Surfaces = () => ({
   `,
 });
 
+export const Borders = () => ({
+  template: `
+  <div class="gl-text-base">
+    <h2 class="gl-heading-2">Borders on containers</h2>
+    <h3 class="gl-heading-4 gl-my-5">Generic (presentational)</h3>
+    <div class="gl-grid gl-grid-cols-3 gl-gap-5">
+      <div class="gl-grid gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-default gl-border gl-border-default">default + default</div>
+      <div class="gl-grid gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-default gl-border gl-border-subtle">default + subtle</div>
+      <div class="gl-grid gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-default gl-border gl-border-strong">default + strong</div>
+      <div class="gl-grid gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-subtle gl-border gl-border-subtle">subtle + subtle</div>
+      <div class="gl-grid gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-strong gl-border gl-border-strong">strong + strong</div>
+    </div>
+    <h3 class="gl-heading-4 gl-my-5">Semantic</h3>
+    <div class="gl-grid gl-grid-cols-3 gl-gap-5">
+      <div class="gl-grid gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-section gl-border gl-border-section">section + section</div>
+    </div>
+    <h2 class="gl-heading-2 gl-mt-5">Borders as dividers</h2>
+    <h3 class="gl-heading-4 gl-my-5">Generic (presentational)</h3>
+    <div class="gl-grid gl-grid-cols-3 gl-gap-5">
+      <div class="gl-grid gl-gap-5 gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-default">
+        on default
+        <div class="gl-w-full gl-border-b gl-border-default"></div>
+        <div class="gl-w-full gl-border-b gl-border-subtle"></div>
+        <div class="gl-w-full gl-border-b gl-border-strong"></div>
+        <div class="gl-w-full gl-border-b gl-border-section"></div>
+      </div>
+      <div class="gl-grid gl-gap-5 gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-subtle">
+        on subtle
+        <div class="gl-w-full gl-border-b gl-border-default"></div>
+        <div class="gl-w-full gl-border-b gl-border-subtle"></div>
+        <div class="gl-w-full gl-border-b gl-border-strong"></div>
+        <div class="gl-w-full gl-border-b gl-border-section"></div>
+      </div>
+      <div class="gl-grid gl-gap-5 gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-strong">
+        on strong
+        <div class="gl-w-full gl-border-b gl-border-default"></div>
+        <div class="gl-w-full gl-border-b gl-border-subtle"></div>
+        <div class="gl-w-full gl-border-b gl-border-strong"></div>
+        <div class="gl-w-full gl-border-b gl-border-section"></div>
+      </div>
+    </div>
+    <h3 class="gl-heading-4 gl-my-5">Semantic</h3>
+    <div class="gl-grid gl-grid-cols-3 gl-gap-5">
+      <div class="gl-grid gl-gap-5 gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-overlap">
+        on overlap
+        <div class="gl-w-full gl-border-b gl-border-default"></div>
+        <div class="gl-w-full gl-border-b gl-border-subtle"></div>
+        <div class="gl-w-full gl-border-b gl-border-strong"></div>
+        <div class="gl-w-full gl-border-b gl-border-section"></div>
+      </div>
+      <div class="gl-grid gl-gap-5 gl-place-items-center gl-p-5 gl-rounded-lg gl-bg-section">
+        on section
+        <div class="gl-w-full gl-border-b gl-border-default"></div>
+        <div class="gl-w-full gl-border-b gl-border-subtle"></div>
+        <div class="gl-w-full gl-border-b gl-border-strong"></div>
+        <div class="gl-w-full gl-border-b gl-border-section"></div>
+      </div>
+    </div>
+  </div>
+  `,
+});
+
 export const Section = () => ({
   template: `
     <div class="gl-border gl-border-section gl-rounded-lg gl-overflow-hidden gl-text-base">
