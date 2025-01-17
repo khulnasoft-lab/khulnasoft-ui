f53fdd4fc9d0ee3bfa01ad19cc209f54612ccda6
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2025-01-17 11:08:17 -0500

    refactor(GlCollapse): remove `BCollapse` component
    BREAKING CHANGE:
    
    `GlCollapse` now has its own implementation and does not rely on
    `BCollapse` anymore.
    
    Drops support of the `toggle` directive for toggling the `GlCollapse`'s
    state. Going forward, the `v-model` should be used instead.
    Drops support of `hide`, `show` events.
    
    Also drops supports of:
    - `appear` prop
    - `v-b-toggle`, which can be used to trigger multiple collapse elements
    - turn a group of collapse components into an accordion with `accordion`
     prop
    - navbar support with `is-nav` prop
    - $root event
    - `show` and `hide` events
    
    See bootstrap doc
    (https://bootstrap-vue.org/docs/components/collapse#collapse) for
    details.

--- src/components/base/collapse/collapse.stories.js
+++ src/components/base/collapse/collapse.stories.js
@@ -1,22 +1,18 @@
-import Vue from 'vue';
-import { GlCollapseToggleDirective } from '../../../directives/collapse_toggle';
 import GlButton from '../button/button.vue';
 import GlCard from '../card/card.vue';
-import BVueReadme from '../../../vendor/bootstrap-vue/src/components/collapse/README.md';
 import GlCollapse from './collapse.vue';
 import readme from './collapse.md';
-
-Vue.directive('gl-collapse-toggle', GlCollapseToggleDirective);
 
 const generateProps = ({ visible = false } = {}) => ({ visible });
 
 const template = `
   <div>
     <h1>Here's a headline</h1>
-    <gl-button v-gl-collapse-toggle.collapse class="gl-float-right" category="primary">
+    <gl-button class="gl-float-right" category="primary" @click="toggle" :aria-expanded="visible ? 'true' : 'false'"
+      aria-controls="collapse-4">
       Toggle Collapse
     </gl-button>
-    <gl-collapse :visible="visible" id="collapse" class="gl-mt-2">
+    <gl-collapse :visible="visible" class="gl-mt-2" id="collapse-4">
       <span>
         This content can be hidden by default, which is good if there are some extensive details
         that should only be visible if the user wants to interact with them
@@ -24,9 +20,14 @@ const template = `
     </gl-collapse>
   </div>`;
 
-const Template = (args, { argTypes }) => ({
+const Template = (args, { argTypes, updateArgs }) => ({
   components: { GlButton, GlCard, GlCollapse },
   props: Object.keys(argTypes),
+  methods: {
+    toggle() {
+      updateArgs({ ...args, visible: !this.visible });
+    },
+  },
   template,
 });
 
@@ -37,8 +38,6 @@ export default {
   title: 'base/collapse',
   component: GlCollapse,
   parameters: {
-    bootstrapComponent: 'b-collapse',
-    bootstrapDocs: BVueReadme,
     docs: {
       description: {
         component: readme,
--- src/components/base/collapse/collapse.vue
+++ src/components/base/collapse/collapse.vue
@@ -1,12 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script>
-import { BCollapse } from '../../../vendor/bootstrap-vue/src/components/collapse/collapse';
-
 export default {
   name: 'GlCollapse',
-  components: {
-    BCollapse,
-  },
   model: {
     prop: 'visible',
     event: 'input',
@@ -17,13 +12,124 @@ export default {
       default: false,
       required: false,
     },
+    tag: {
+      type: String,
+      required: false,
+      default: 'div',
+    },
+  },
+  data() {
+    return {
+      show: this.visible,
+      transitioning: false,
+    };
+  },
+  computed: {
+    classObject() {
+      const { transitioning } = this;
+
+      return {
+        collapse: !transitioning,
+        show: this.show && !transitioning,
+      };
+    },
+    slotScope() {
+      return {
+        visible: this.show,
+        close: () => {
+          this.show = false;
+        },
+      };
+    },
+    transitionProps() {
+      return {
+        ...this.$attrs,
+        css: true,
+        enterClass: '',
+        enterActiveClass: 'collapsing',
+        enterToClass: 'collapse show',
+        leaveClass: 'collapse show',
+        leaveActiveClass: 'collapsing',
+        leaveToClass: 'collapse',
+      };
+    },
+  },
+  watch: {
+    visible(newValue) {
+      if (newValue !== this.show) {
+        this.show = newValue;
+      }
+    },
+    show(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        this.emitState();
+      }
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.emitState();
+    });
+  },
+  beforeDestroy() {
+    // Trigger state emit if needed
+    this.show = false;
+  },
+  methods: {
+    reflow(el) {
+      // Requesting an elements offsetHeight will trigger a reflow of the element content
+      // Without forcing a reflow, the browser optimize the operation and cause animation to fail
+      return Boolean(el && el.nodeType === Node.ELEMENT_NODE) && el.offsetHeight;
+    },
+    emitState() {
+      const { show } = this;
+      this.$emit('input', show);
+    },
+    enter(el) {
+      this.transitioning = true;
+
+      el.style.height = 0;
+      // In a `debounceByAnimationFrame()` for `appear` to work
+      window.requestAnimationFrame(() => {
+        this.reflow(el);
+        el.style.height = `${el.scrollHeight}px`;
+      });
+    },
+    afterEnter(el) {
+      this.transitioning = false;
+      this.$emit('shown');
+      el.style.height = '';
+    },
+    leave(el) {
+      this.transitioning = true;
+      el.style.height = 'auto';
+      el.style.display = 'block';
+      el.style.height = `${el.getBoundingClientRect().height}px`;
+      this.reflow(el);
+      el.style.height = 0;
+    },
+    afterLeave(el) {
+      this.transitioning = false;
+      this.$emit('hidden');
+      el.style.height = '';
+    },
   },
 };
 </script>
 
 <template>
-  <b-collapse :visible="visible" v-bind="$attrs" v-on="$listeners">
-    <!-- @slot The content to show/hide. -->
-    <slot></slot>
-  </b-collapse>
+  <transition
+    :visible="visible"
+    v-bind="transitionProps"
+    v-on="$listeners"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @leave="leave"
+    @afterLeave="afterLeave"
+  >
+    <component :is="tag" v-show="show" :class="classObject">
+      <!-- @slot The content to show/hide. -->
+      <slot v-bind="slotScope"></slot>
+    </component>
+  </transition>
 </template>
