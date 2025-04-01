267576afceed98db9f815eb96c129cb55bb786b4
Thomas Hutterer <thutterer@gitlab.com>
2025-04-01 16:48:20 +0200

    chore: Remove BNavItemDropdown component
    This Bootstrap component is not in use anymore.

--- src/vendor/bootstrap-vue/src/components/nav/index.js
+++ src/vendor/bootstrap-vue/src/components/nav/index.js
@@ -1,5 +1,4 @@
 import { BNav } from './nav'
 import { BNavItem } from './nav-item'
-import { BNavItemDropdown } from './nav-item-dropdown'
 
-export { BNav, BNavItem, BNavItemDropdown }
+export { BNav, BNavItem }
