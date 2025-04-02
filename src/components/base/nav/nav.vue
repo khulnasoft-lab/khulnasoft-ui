132aff19c80c403649b001f869f88215f826516c
Thomas Hutterer <thutterer@gitlab.com>
2025-04-02 14:07:27 +0200

    refactor(GlNav): Remove dependency on BNav

--- src/components/base/nav/nav.vue
+++ src/components/base/nav/nav.vue
@@ -1,17 +1,48 @@
 <script>
-import { BNav } from '../../../vendor/bootstrap-vue/src/components/nav/nav';
-
 export default {
   name: 'GlNav',
-  components: {
-    BNav,
+  props: {
+    align: { type: String, required: false, default: '' },
+
+    // Set to `true` if placing in a card header
+    cardHeader: { type: Boolean, required: false },
+
+    fill: { type: Boolean, required: false },
+    justified: { type: Boolean, required: false },
+    pills: { type: Boolean, required: false },
+    small: { type: Boolean, required: false },
+    tabs: { type: Boolean, required: false },
+    tag: { type: String, required: false, default: 'ul' },
   },
-  inheritAttrs: false,
+  computed: {
+    justifyContent() {
+      if (!this.align) return '';
+
+      const alignMapping = {
+        left: 'start',
+        right: 'end',
+      };
+
+      return `justify-content-${alignMapping[this.align] || this.align}`;
+    },
+    classes() {
+      return {
+        'nav-tabs': this.tabs,
+        'nav-pills': this.pills && !this.tabs,
+        'card-header-tabs': this.cardHeader && this.tabs,
+        'card-header-pills': this.cardHeader && this.pills && !this.tabs,
+        'nav-fill': this.fill,
+        'nav-justified': this.justified,
+        [this.justifyContent]: this.align,
+        small: this.small,
+      };
+    },
+  },
 };
 </script>
 
 <template>
-  <b-nav v-bind="$attrs" v-on="$listeners">
+  <component :is="tag" class="nav" :class="classes">
     <slot></slot>
-  </b-nav>
+  </component>
 </template>
