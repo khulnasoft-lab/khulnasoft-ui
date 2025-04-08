a446b71439ca5ad5e37e236402231e74be89660a
Thomas Hutterer <thutterer@gitlab.com>
2025-04-08 13:49:19 +0200

    chore(GlNav): Add comments to props

--- src/components/base/nav/nav.vue
+++ src/components/base/nav/nav.vue
@@ -2,16 +2,38 @@
 export default {
   name: 'GlNav',
   props: {
+    /**
+     * Align the nav items in the nav: 'start' (or 'left'), 'center', 'end' (or 'right')
+     */
     align: { type: String, required: false, default: '' },
-
-    // Set to `true` if placing in a card header
+    /**
+     * Set this prop when the nav is placed inside a card header
+     */
     cardHeader: { type: Boolean, required: false, default: false },
-
+    /**
+     * Proportionately fills all horizontal space with nav items.
+     * All horizontal space is occupied, but not every nav item has the same width
+     */
     fill: { type: Boolean, required: false, default: false },
+    /**
+     * Fills all horizontal space with nav items, but unlike 'fill', every nav item will be the same width
+     */
     justified: { type: Boolean, required: false, default: false },
+    /**
+     * Renders the nav items with the appearance of pill buttons
+     */
     pills: { type: Boolean, required: false, default: false },
+    /**
+     * Makes the nav smaller
+     */
     small: { type: Boolean, required: false, default: false },
+    /**
+     * Renders the nav items with the appearance of tabs
+     */
     tabs: { type: Boolean, required: false, default: false },
+    /**
+     * Specify the HTML tag to render instead of the default tag
+     */
     tag: { type: String, required: false, default: 'ul' },
   },
   computed: {
